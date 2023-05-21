4798617e1c524568b5e2cba4cab83cf2f0d72f71
suankun <edmont@abv.bg>
2023-05-21 10:39:42 +0300

    add snippets

---
+++ Codewars/8kyu/L1SetAlarm.cpp
@@ -0,0 +1,8 @@
+bool set_alarm(const bool& employed,const bool& vacation){
+  return (employed == true && vacation == false);
+}
+//////////////////
+bool set_alarm( const bool& employed, const bool& vacation )
+{
+  return (employed && !vacation);
+}
