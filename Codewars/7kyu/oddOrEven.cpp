582ac329e54581b64368953cda9005bc2cedb90f
suankun <edmont@abv.bg>
2023-05-23 20:24:44 +0300

    add snippets

---
+++ Codewars/7kyu/oddOrEven.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <vector>
+
+std::string odd_or_even(const std::vector<int> &arr)
+{
+  int sum {};
+  for (auto num: arr) sum += num;
+  if (sum % 2 == 0) return "even";
+  else return "odd";
+}
+////////////////
+#include <string>
+#include <vector>
+#include <numeric>
+
+std::string odd_or_even(const std::vector<int> &arr)
+{
+    return std::accumulate(arr.begin(),arr.end(),0)&1?"odd":"even";
+}
---
+++ Codewars/8kyu/thinkful-LogicDrillsTrafficLight.cpp
@@ -0,0 +1,16 @@
+#include <string>
+
+std::string update_light(std::string current) {
+  std::string result;
+  if (current == "green") result = "yellow";
+  else if (current == "yellow") result = "red";
+  else result = "green";
+  return result;
+}
+/////////////////
+#include <string>
+#include <map>
+std::string update_light(std::string current) {
+  std::map<std::string, std::string> mp {{"green", "yellow"}, {"yellow", "red"}, {"red", "green"}};
+  return mp[current];
+}
