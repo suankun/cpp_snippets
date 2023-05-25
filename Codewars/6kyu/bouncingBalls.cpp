82cbb2e9e636569619cabfa71081edf664c55a48
suankun <edmont@abv.bg>
2023-05-25 09:45:36 +0300

    add snippets

---
+++ Codewars/6kyu/bouncingBalls.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+class Bouncingball
+{
+public:
+    static int bouncingBall(double h, double bounce, double window);
+};
+
+int Bouncingball::bouncingBall(double h, double bounce, double window) {
+    if (h <= 0 || bounce >= 1 || bounce <= 0 || window >= h) return -1;
+
+    int ballSeen {0};
+    while (h >= window)
+    {
+        ballSeen++;
+        h *= bounce;
+        if (h <= window) break;
+        ballSeen++;
+    }
+    
+    return ballSeen;
+};
+
+int main() {
+    Bouncingball one;
+    double result = one.bouncingBall(3, 0.66, 1.5); // 3
+    // double result = one.bouncingBall(30, 0.66, 1.5); // 15
+
+    cout << result << endl;
+
+    return 0;
+}
+
+/////////////////////////////////////////////////////
