5123b19b9e4f5ede520a54a48ab7f4aea8b6322d
suankun <edmont@abv.bg>
2023-05-25 13:08:21 +0300

    add snippets

---
+++ Codewars/8kyu/DNAtoRNAconversion.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+std::string DNAtoRNA(std::string dna){
+  std::string res;
+  for (size_t i {}; i < dna.length(); i++) {
+    if (dna[i] == 'T') res += 'U';
+    else res += dna[i];
+  }
+  return res;
+}
+
+int main() {
+    string dna {"GCAT"}; // GCAU
+    string result = DNAtoRNA(dna);
+
+    cout << result << endl;
+
+    return 0;
+}
+
+/////////////////////////////////////////////////////
+std::string DNAtoRNA(std::string dna){
+  std::replace(dna.begin(), dna.end(), 'T', 'U');
+  return dna;
+}
