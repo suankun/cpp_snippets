af9fa5afededa253d9b7ffca3229b70a43c2bacd
suankun <edmont@abv.bg>
2023-05-27 22:15:56 +0300

    add snippets

---
+++ Codewars/8kyu/Count by X.cpp
@@ -0,0 +1,10 @@
+// https://www.codewars.com/kata/5513795bd3fafb56c200049e/train/cpp
+
+#include <vector>
+std::vector<int> countBy(int x,int n){
+  std::vector <int> res;
+  for (int i {1}; i <= n; i++) {
+    res.push_back(x*i);
+  }
+  return res;
+}
