b796f7a9fced3281ea3d9d4e179f9f779f809701
suankun <edmont@abv.bg>
2023-05-29 21:52:04 +0300

    add snippets

---
+++ Codewars/8kyu/Even or Odd.cpp
@@ -0,0 +1,8 @@
+// https://www.codewars.com/kata/53da3dbb4a5168369a0000fe/train/cpp
+
+#include <string>
+
+std::string even_or_odd(int number) 
+{
+  return number%2==0 ? "Even" : "Odd";
+}
---
+++ Codewars/8kyu/Invert values.cpp
@@ -0,0 +1,29 @@
+// https://www.codewars.com/kata/5899dc03bc95b1bf1b0000ad/solutions/cpp
+
+#include <vector>
+
+std::vector<int> invert(std::vector<int> values)
+{
+  std::vector<int> res;
+    for (auto val: values) res.push_back(val*(-1));
+    return res;
+}
+
+/////////
+#include <vector>
+#include <algorithm>
+
+std::vector<int> invert(std::vector<int> values)
+{
+    transform(values.begin(), values.end(), values.begin(), std::negate<int>());;
+    return values;
+}
+///////////
+#include <vector>
+
+std::vector<int> invert(std::vector<int> values)
+{
+    for( int i = 0; i < values.size(); i++)
+      values[i] *= -1;
+      return values;
+}
