c29b301119d919f599e08fbb1f2a262a356fc262
suankun <edmont@abv.bg>
2023-05-29 22:34:29 +0300

    add snippets

---
+++ Codewars/8kyu/Exclamation marks series #11 Replace all vowel to exclamation mark in the sentence.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+// https://www.codewars.com/kata/57fb09ef2b5314a8a90001ed/train/cpp
+
+string replace(const string &s)
+{
+    string clear_from_vowels;
+    string vowels {"aeiouAEIOU"};
+    for (size_t i {}; i < s.length(); i++) {
+        size_t found = vowels.find(s[i]);
+        if (found != string::npos) clear_from_vowels += '!';
+        else clear_from_vowels += s[i];
+    }
+    return clear_from_vowels;
+}
+
+int main() {
+    string hi {"Hi!"};
+    cout << replace(hi) << endl;
+
+    return 0;
+}
+
+/////////////////
+#include <string>
+#include <regex>
+
+using namespace std;
+
+string replace(const string &s)
+{
+  return regex_replace(s, regex("[aeiouAEIOU]"), "!");
+}
+
