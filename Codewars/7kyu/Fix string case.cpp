b5e10f86123ccec1d3b10160ceab30e924deca08
suankun <edmont@abv.bg>
2023-05-30 10:38:39 +0300

    add snippets

---
+++ Codewars/7kyu/Fix string case.cpp
@@ -0,0 +1,37 @@
+// https://www.codewars.com/kata/5b180e9fedaa564a7000009a/train/cpp
+#include <iostream>
+using namespace std;
+
+
+#include <string>
+
+std::string solve(const std::string& str){
+    size_t cnt {};
+    std::string fixed_string;
+    for (size_t i {}; i<str.length(); i++) {
+        if (str[i] >= 'A' && str[i] <= 'Z') cnt++;
+    }
+    if (cnt > str.length()/2) {
+        for (size_t k {}; k<str.length(); k++) {
+            fixed_string.push_back(std::toupper(str[k]));
+        }
+    } else {
+
+        for (size_t j {}; j<str.length(); j++) {
+            fixed_string.push_back(std::tolower(str[j]));
+        }
+    }
+    return fixed_string;
+}
+
+int main() {
+    // string one1 {"code"}; // code
+    // string one2 {"CODe"}; // CODE
+    string one3 {"UKKCSQVEVSIVPZRSU"}; // ukkcsqvevsivpzrsu
+    // cout << solve(one1) << endl;
+    // cout << solve(one2) << endl;
+    cout << solve(one3) << endl;
+
+    return 0;
+}
+
