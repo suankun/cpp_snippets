1600dca7ff4b092aa7b6a1393df62e5637d9709f
suankun <edmont@abv.bg>
2023-05-31 16:24:37 +0300

    add snippets

---
+++ Codewars/7kyu/Remove anchor from URL.cpp
@@ -0,0 +1,27 @@
+// https://www.codewars.com/kata/51f2b4448cadf20ed0000386/train/cpp
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+std::string replaceAll(std::string str) {
+    std::string res;
+    for (size_t i {}; i<str.length(); i++) {
+        if (str[i] == '#') break;
+        res.push_back(str[i]);
+    }
+    return res;  
+}
+
+int main() {
+    
+    string one = {"www.codewars.com#about"};
+    string res = replaceAll(one);
+    std::cout << res << endl;
+
+    return 0;
+}
+///////////////
+std::string replaceAll(std::string str) {
+  return str.substr(0, str.find('#'));
+}
