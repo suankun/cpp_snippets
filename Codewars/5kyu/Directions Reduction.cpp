9a8f9b41801de0a523ddbf41828fddeca6c4bcb6
suankun <edmont@abv.bg>
2023-06-01 22:37:46 +0300

    add snippets

--- Codewars/5kyu/Directions Reduction.cpp
+++ Codewars/5kyu/Directions Reduction.cpp
@@ -6,25 +6,22 @@ using namespace std;
 
 class DirReduction {
 
-public: static std::vector<std::string> dirReduc(std::vector<std::string> &arr)
-          {
-                std::vector<std::string> res; 
-                for (std::string & str : arr) 
-                    { 
-                          if (res.empty()) res.push_back(str); 
-                          else 
-                          { 
-                            switch (str[0]) 
-                                { 
-                                  case 'N' : (res.back()[0] == 'S') ? res.pop_back() : res.push_back(str); break; 
-                                  case 'S' : (res.back()[0] == 'N') ? res.pop_back() : res.push_back(str); break;
-                                  case 'W' : (res.back()[0] == 'E') ? res.pop_back() : res.push_back(str); break; 
-                                  case 'E' : (res.back()[0] == 'W') ? res.pop_back() : res.push_back(str); break; 
-                                } 
-                         }
-                    } 
-                  return res; 
+public:
+    static std::vector<std::string> dirReduc(std::vector<std::string> &arr) {
+        std::vector<std::string> res; 
+        for (std::string & str : arr) { 
+            if (res.empty()) res.push_back(str); 
+            else { 
+                switch (str[0]) { 
+                case 'N' : (res.back()[0] == 'S') ? res.pop_back() : res.push_back(str); break; 
+                case 'S' : (res.back()[0] == 'N') ? res.pop_back() : res.push_back(str); break;
+                case 'W' : (res.back()[0] == 'E') ? res.pop_back() : res.push_back(str); break; 
+                case 'E' : (res.back()[0] == 'W') ? res.pop_back() : res.push_back(str); break; 
+                } 
+            }
         } 
+        return res; 
+    } 
 };
 
 int main() {
---
+++ Codewars/7kyu/Find the vowels.cpp
@@ -0,0 +1,15 @@
+// https://www.codewars.com/kata/5680781b6b7c2be860000036/train/cpp
+#include <vector>
+#include <string>
+
+std::vector<int> vowelIndices(const std::string& word)
+{
+    std::vector<int> res;
+    std::string vowels {"aoueiAOUEIyY"};
+    for (size_t i {}; i<word.size(); i++) {
+      for (size_t j {}; j<vowels.length(); j++) {
+        if (word[i] == vowels[j]) res.push_back(i+1);
+      }
+    }
+    return res;
+}
