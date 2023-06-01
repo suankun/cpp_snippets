809c6cb3d4bb0b7136bd8c061d31e5dda232f68f
suankun <edmont@abv.bg>
2023-06-01 15:24:45 +0300

    add snippets

---
+++ Codewars/7kyu/Sort Numbers.cpp
@@ -0,0 +1,20 @@
+// https://www.codewars.com/kata/5174a4c0f2769dd8b1000003/train/cpp
+#include <iostream>
+using namespace std;
+
+#include <vector>
+#include <bits/stdc++.h>
+
+std::vector<int> solution(std::vector<int> nums) {
+    sort(nums.begin(), nums.end());
+    return nums;
+}
+
+int main() {
+    vector<int> nums {3, 5, 1, 2, 4};
+    vector<int> res = solution(nums);
+    for (auto num: res) cout << num << " ";
+    cout << endl;
+
+    return 0;
+}
