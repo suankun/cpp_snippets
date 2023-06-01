4ca1d76b19dfce34b4cb75265b62042b1a4106dc
suankun <edmont@abv.bg>
2023-06-01 15:11:55 +0300

    add snippets

---
+++ Codewars/7kyu/Sum of angles.cpp
@@ -0,0 +1,19 @@
+// https://www.codewars.com/kata/5a03b3f6a1c9040084001765/train/cpp
+int angle(int n) {
+  return  (n-2)*180;
+}
+///////////
+#define 🙌 ( 
+#define 🎁 return 
+#define 🤞 )
+#define 😢 - 
+#define 😁 * 
+#define 🎉 ;
+#define 👀 int 
+#define 🤲 2
+#define 🤚 180
+#define 🤷‍ {
+#define 🤦‍ }
+#define 🤔 angle
+
+👀 🤔 🙌 👀 🎶 🤞 🤷‍ 🎁 🙌 🎶 😢 🤲 🤞 😁 🤚 🎉 🤦‍
