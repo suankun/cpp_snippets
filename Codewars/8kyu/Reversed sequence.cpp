8717747175f4e37e91c1548c1f7e25829108b426
suankun <edmont@abv.bg>
2023-06-03 17:21:56 +0300

    add snippets

---
+++ Codewars/8kyu/Reversed sequence.cpp
@@ -0,0 +1,13 @@
+// https://www.codewars.com/kata/5a00e05cc374cb34d100000d/train/cpp
+#include <vector>
+std::vector<int> reverseSeq(int n) {
+  std::vector<int> res;
+  for (int i {n}; i>0; i--) res.push_back(i);
+  return res;
+}
+////////////
+std::vector<int> reverseSeq(int n) {
+  std::vector<int> v;
+  while ( n ) v.push_back(n--);
+  return v;
+}
