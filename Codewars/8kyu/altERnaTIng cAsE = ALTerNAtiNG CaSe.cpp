593170fdca84a448b7c389c5fac4e2ae4ff9dc29
suankun <edmont@abv.bg>
2023-06-04 11:49:31 +0300

    add snippets

---
+++ Codewars/8kyu/altERnaTIng cAsE = ALTerNAtiNG CaSe.cpp
@@ -0,0 +1,30 @@
+// https://www.codewars.com/kata/56efc695740d30f963000557/train/cpp
+#include <iostream>
+#include <string>
+using namespace std;
+
+std::string to_alternating_case(const std::string& str)
+{
+    std::string res = str;
+    for (size_t i {}; i<res.length(); i++) {
+        if (isupper(res[i])) res[i] = towlower(res[i]);
+        else res[i] = toupper(res[i]);
+    }
+	return res;
+}
+
+int main() {
+    std::string text {"HeLLo WoRLD"};
+    std::cout << to_alternating_case(text);
+
+    return 0;
+}
+///////////////
+std::string to_alternating_case(const std::string& str)
+{
+    std::string res = str;
+    for (size_t i {}; i<res.length(); i++) {
+        isupper(res[i]) ? res[i] = towlower(res[i]) : res[i] = toupper(res[i]);
+    }
+	  return res;
+}
