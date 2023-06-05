77e0e42b54f96c8eebdb84b642ae25c489339bb6
suankun <edmont@abv.bg>
2023-06-05 09:13:36 +0300

    add snippets

---
+++ Codewars/7kyu/Sum of Triangular Numbers.cpp
@@ -0,0 +1,17 @@
+// https://www.codewars.com/kata/580878d5d27b84b64c000b51/train/cpp
+int sumTriangularNumbers(int n) {
+    if (n<=0) return 0;
+    int cnt {}, sum {};
+    for (int i {}; i<n; i++) {
+        for (int j {}; j<=i; j++) {
+            cnt++;
+        }
+        sum += cnt;
+    }
+    return sum;
+}
+//////////
+// unsigned int sumTriangularNumbers(const int n)
+// {
+//   return n<0 ? 0 : n*(n+1)*(n+2)/6 ;
+// }
