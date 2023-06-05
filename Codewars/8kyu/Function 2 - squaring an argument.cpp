8302e9eceb5e9f4d6f95c9a48355bb854b2f4e59
suankun <edmont@abv.bg>
2023-06-05 09:36:36 +0300

    add snippets

---
+++ Codewars/8kyu/Function 2 - squaring an argument.cpp
@@ -0,0 +1,9 @@
+// https://www.codewars.com/kata/523b623152af8a30c6000027/train/cpp
+int square(int num) {
+  return num*num;
+}
+///////
+template <typename T> 
+T square(const T a) {
+  return a * a;
+}
