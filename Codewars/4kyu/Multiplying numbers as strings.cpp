03f26e43cff58f9c1734a937e53d926715a2c72c
suankun <edmont@abv.bg>
2023-06-09 22:43:38 +0300

    add snippets

--- Codewars/4kyu/Multiplying numbers as strings.cpp
+++ Codewars/4kyu/Multiplying numbers as strings.cpp
@@ -31,28 +31,28 @@ string multiply(string a, string b) {
     return "0";
 }
 //////////////
-#include <iostream>
-#include <string>
+// #include <iostream>
+// #include <string>
 
-using namespace std;
+// using namespace std;
 
-string multiply(const string &t, const string &b)
-{
-    int tsize = static_cast<int>(t.size());
-    int bsize = static_cast<int>(b.size());
+// string multiply(const string &t, const string &b)
+// {
+//     int tsize = static_cast<int>(t.size());
+//     int bsize = static_cast<int>(b.size());
   
-    vector<size_t> pos(static_cast<size_t>(tsize + bsize), 0);
-    for (int i = tsize - 1; i >= 0; i--){
-        for (int j = bsize - 1; j >= 0; j--){
-            int mul = (t[i] - '0') * (b[j] - '0');
-            int p1 = i + j, p2 = i + j + 1;
-            int sum = mul + pos[p2];
-            pos[p1] += sum / 10;
-            pos[p2] = sum % 10;
-        }
-    }
-    string res;
-    for (const auto& p : pos) if (!(res.size() == 0 && p == 0)) res.push_back('0'+p);
+//     vector<size_t> pos(static_cast<size_t>(tsize + bsize), 0);
+//     for (int i = tsize - 1; i >= 0; i--){
+//         for (int j = bsize - 1; j >= 0; j--){
+//             int mul = (t[i] - '0') * (b[j] - '0');
+//             int p1 = i + j, p2 = i + j + 1;
+//             int sum = mul + pos[p2];
+//             pos[p1] += sum / 10;
+//             pos[p2] = sum % 10;
+//         }
+//     }
+//     string res;
+//     for (const auto& p : pos) if (!(res.size() == 0 && p == 0)) res.push_back('0'+p);
     
-    return (res.size() == 0)? "0" : res;
-}
+//     return (res.size() == 0)? "0" : res;
+// }
