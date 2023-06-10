bb43fd2a6166e1dafca33973bda01aeac4dcc1b3
suankun <edmont@abv.bg>
2023-06-10 13:19:02 +0300

    add snippets

---
+++ Codewars/7kyu/The Office IV - Find a Meeting Room.cpp
@@ -0,0 +1,9 @@
+// codewars.com/kata/57f604a21bd4fe771b00009c/train/cpp
+#include <vector>
+
+int meeting(const std::vector<char>& rooms) {
+  for (size_t i {}; i<rooms.size(); i++) {
+    if (rooms[i]=='O') return i;
+  }
+  return -1;
+}
