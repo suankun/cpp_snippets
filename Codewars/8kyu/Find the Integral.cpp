b6670c0fc2d20b7fe7dc92a1c16dcb3bb4bded37
suankun <edmont@abv.bg>
2023-06-11 10:20:26 +0300

    add snippets

---
+++ Codewars/8kyu/Find the Integral.cpp
@@ -0,0 +1,8 @@
+// https://www.codewars.com/kata/59811fd8a070625d4c000013/train/cpp
+#include <string>
+std::string integrate(const int& coefficient,const int& exponent){
+  int exp = exponent+1;
+  int result_int = coefficient/exp;
+  std::string res = std::to_string(result_int) + "x^" + std::to_string(exp);
+  return res;
+}
