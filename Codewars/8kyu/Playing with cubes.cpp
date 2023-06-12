18c91b9e893cda4251d222b717be904ac5bef699
suankun <edmont@abv.bg>
2023-06-12 23:33:16 +0300

    add snippets

---
+++ Codewars/8kyu/Playing with cubes.cpp
@@ -0,0 +1,33 @@
+// https://www.codewars.com/kata/55c0a79e20be94c91400014b/train/cpp
+class Cube {
+private:
+  int Side {};
+  
+public:
+  int GetSide() {
+    return Side;
+  }
+  void SetSide(int user_side) {
+    Side = user_side;
+  }
+};
+/////////////
+class Cube
+{
+  private:
+    int Side = 0;
+   
+  public:
+    int GetSide();
+    void SetSide(int);
+};
+
+int Cube::GetSide()
+{
+  return this->Side;
+}
+
+void Cube::SetSide(int n)
+{
+  this->Side = n;
+}
