c402cd2f52fd7a035fa2fedb3b1cf65462719f99
suankun <edmont@abv.bg>
2023-06-15 20:15:23 +0300

    add snippets

---
+++ Codewars/8kyu/Remove String Spaces.cpp
@@ -0,0 +1,18 @@
+// https://www.codewars.com/kata/57eae20f5500ad98e50002c5/train/cpp
+#include <string>
+
+std::string no_space(const std::string& x)
+{
+  std::string result;
+  for (size_t i {}; i<x.length(); i++) {
+    if (x[i] == ' ') continue;
+    else result+=x[i];
+  }
+   return result;
+}
+/////////
+// std::string no_space(std::string x)
+// {
+//     x.erase(std::remove(x.begin(), x.end(), ' '), x.end());
+//     return x;
+// }
