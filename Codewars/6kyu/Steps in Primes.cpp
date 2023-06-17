8422e9ddba37acd18555e2feec369b20702b78bb
suankun <edmont@abv.bg>
2023-06-17 22:07:53 +0300

    add snippets

--- Codewars/6kyu/Steps in Primes.cpp
+++ Codewars/6kyu/Steps in Primes.cpp
@@ -36,3 +36,28 @@ int main() {
 
     return 0;
 }
+//////////
+#include <cmath>
+#include <iostream>
+
+class StepInPrimes
+{
+public:
+    static bool isPrime(long long n) {
+        for (int i = 2; i <= sqrt(n); i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+    
+        return true;
+    }
+    
+    static std::pair <long long, long long> step(int g, long long m, long long n) {
+        for(m; m <= n; m++) {
+            if (isPrime(m) && isPrime(m + g)) 
+                return {m, m + g};
+        }
+        return {0, 0};
+    }
+};
