8475c326b31a0e2b2e78e94b53a6a270fe72ce8a
suankun <edmont@abv.bg>
2023-06-21 18:02:36 +0300

    add snippets

--- Codewars/7kyu/Parts of a list.cpp
+++ Codewars/7kyu/Parts of a list.cpp
@@ -44,21 +44,21 @@ int main() {
     return 0;
 }
 ///////////////////
-using namespace std;
+// using namespace std;
 
-class PartList
-{
-public:
-    static std::vector<std::pair <std::string, std::string>> partlist(std::vector<std::string> &arr)
-    {
-      vector<pair<string, string>> result;
-      auto combine = [](string a, string b) { return a + ' ' + b; };
-      for (int i = 1; i < arr.size(); i++)
-      {
-        result.emplace_back(
-          accumulate(arr.begin() + 1, arr.begin() + i, arr[0], combine),
-          accumulate(arr.begin() + i + 1,  arr.end(), arr[i], combine));
-      }
-      return result;
-    }
-};
+// class PartList
+// {
+// public:
+//     static std::vector<std::pair <std::string, std::string>> partlist(std::vector<std::string> &arr)
+//     {
+//       vector<pair<string, string>> result;
+//       auto combine = [](string a, string b) { return a + ' ' + b; };
+//       for (int i = 1; i < arr.size(); i++)
+//       {
+//         result.emplace_back(
+//           accumulate(arr.begin() + 1, arr.begin() + i, arr[0], combine),
+//           accumulate(arr.begin() + i + 1,  arr.end(), arr[i], combine));
+//       }
+//       return result;
+//     }
+// };
