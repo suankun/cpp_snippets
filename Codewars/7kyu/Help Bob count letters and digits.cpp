73c68e35fbce11392208362362ca89e9449dc338
suankun <edmont@abv.bg>
2023-07-01 21:18:26 +0300

    add snippets

---
+++ Codewars/7kyu/Help Bob count letters and digits.cpp
@@ -0,0 +1,12 @@
+// https://www.codewars.com/kata/5738f5ea9545204cec000155/train/cpp
+#include <iostream>
+#include <string>
+
+int countLettersAndDigits(std::string input)
+{
+    int count {};
+    for (size_t i {}; i<input.length(); i++) {
+        if (isalpha(input[i]) || isdigit(input[i])) count++;
+    }
+    return count;
+}
