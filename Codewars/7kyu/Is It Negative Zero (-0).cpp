a6340e2ba1f7ece2f3f9e3c2e047dfc874144991
suankun <edmont@abv.bg>
2023-07-06 21:42:42 +0300

    add snippets

---
+++ Codewars/7kyu/Is It Negative Zero (-0).cpp
@@ -0,0 +1,12 @@
+// https://www.codewars.com/kata/5c5086287bc6600001c7589a/train/cpp
+#include <iostream>
+#include <cmath>
+
+bool isNegativeZero(float n) {
+  std::cout << n << std::endl;
+  return n == 0 && std::signbit(n) ? true : false;
+}
+//
+bool isNegativeZero(float n) {
+  return (1 / n < 0) * !n;
+}
