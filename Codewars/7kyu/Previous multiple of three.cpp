1b22f342b75d7606056a795d4df743057e866fde
suankun <edmont@abv.bg>
2023-07-12 13:50:54 +0300

    add snippets

---
+++ Codewars/7kyu/Previous multiple of three.cpp
@@ -0,0 +1,15 @@
+// https://www.codewars.com/kata/61123a6f2446320021db987d/train/cpp
+int prev_mult_of_three (int n) {
+  if (n%3==0) return n;
+  while (n>0) {
+    n /= 10;
+    if (n%3==0 && n!=0) return n;
+  }
+  return -1;
+}
+//
+int prev_mult_of_three(int n) {
+  while (n % 3 != 0)
+    n /= 10;
+  return n == 0 ? -1 : n;
+}
