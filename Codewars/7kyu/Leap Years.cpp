d6d040f51d2e0e02bb6e28784e4012c05f097711
suankun <edmont@abv.bg>
2023-07-12 21:48:01 +0300

    add snippets

---
+++ Codewars/7kyu/Leap Years.cpp
@@ -0,0 +1,17 @@
+// https://www.codewars.com/kata/526c7363236867513f0005ca/train/cpp
+bool IsLeapYear(int year) {
+    if (year % 400 == 0) {
+        return true;
+    }
+    if (year % 100 == 0) {
+        return false;
+    }
+    if (year % 4 == 0) {
+        return true;
+    }
+    return false;
+}
+//
+bool IsLeapYear(int year) {
+  return year % 4 == 0 && (year % 100 != 0 || year % 400 == 0);
+}
