bee9d0537f7b3c2cf4c21a4b6de329cffe7c9296
suankun <edmont@abv.bg>
2023-07-13 21:41:39 +0300

    add snippets

---
+++ Codewars/8kyu/Jenny's secret message.cpp
@@ -0,0 +1,15 @@
+// https://www.codewars.com/kata/55225023e1be1ec8bc000390/train/cpp
+#include <string>
+
+std::string greet(std::string name) 
+{
+  if(name == "Johnny") {
+    return "Hello, my love!";
+  }
+  return "Hello, " + name + "!";
+}
+//
+std::string greet(std::string name) 
+{
+  return "Hello, " + ( name == "Johnny" ? "my love" : name) + "!";
+}
