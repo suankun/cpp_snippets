5f58e4f5ae86fc6c85e3c43c5063bc61be14a22f
suankun <edmont@abv.bg>
2023-07-18 20:49:33 +0300

    add snippets

---
+++ Codewars/7kyu/Find the index of the second occurrence of a letter in a string.cpp
@@ -0,0 +1,20 @@
+// https://www.codewars.com/kata/63f96036b15a210058300ca9/train/cpp
+#include <string>
+
+int secondSymbol(const std::string& str, char symbol) {
+  int counter {}, index {};
+  for (size_t i {}; i<str.length(); i++) {
+    if (str[i] == symbol) counter++;
+    if (counter == 2) {
+      index = i;
+      break;
+    }
+  }
+  return counter == 2 ? index : -1;
+}
+//
+#include <string>
+
+int secondSymbol(const std::string& str, char symbol) {
+  return str.find(symbol, str.find(symbol) + 1);
+}
