2fddc884263b9dc2ebfcefd5c5abb43270577bcf
suankun <edmont@abv.bg>
2023-07-22 20:44:29 +0300

    add snippets

---
+++ Codewars/8kyu/Get Nth Even Number.cpp
@@ -0,0 +1,9 @@
+// https://www.codewars.com/kata/5933a1f8552bc2750a0000ed/train/cpp
+int nthEven(int n) {
+  if (n == 1) return 0;
+  return n*2 - 2;
+}
+//
+int nthEven(int n) {
+  return n * 2 - 2;
+}
