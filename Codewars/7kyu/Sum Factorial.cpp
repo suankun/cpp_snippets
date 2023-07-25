71a688179a42880261c5aa3252f4bc0f20df5117
suankun <edmont@abv.bg>
2023-07-25 23:11:12 +0300

    add snippets

---
+++ Codewars/7kyu/Sum Factorial.cpp
@@ -0,0 +1,16 @@
+// https://www.codewars.com/kata/56b0f6243196b9d42d000034/train/cpp
+#include <vector>
+
+unsigned long long int sum_factorial(std::vector<int> vi){
+  
+  unsigned long long int temp_sum {1}, total_sum {};
+  for (size_t i {}; i<vi.size(); i++) {
+    unsigned long long int temp_num = vi[i];
+    for (unsigned long long int j {temp_num}; j>0; j--) {
+      temp_sum *= j;
+    }
+    total_sum += temp_sum;
+    temp_sum = 1;
+  }
+  return total_sum;
+}
