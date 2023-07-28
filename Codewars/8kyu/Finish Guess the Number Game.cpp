aa677158c5ff0816cd3f315b9467e3e1957c658c
suankun <edmont@abv.bg>
2023-07-28 16:47:49 +0300

    add snippets

---
+++ Codewars/8kyu/Finish Guess the Number Game.cpp
@@ -0,0 +1,33 @@
+// https://www.codewars.com/kata/568018a64f35f0c613000054/train/cpp
+#include <stdexcept>
+
+class Guesser
+{
+public:
+    Guesser(int number, int lives)
+        : number(number), lives(lives)
+    {
+    }
+
+    bool guess(int n)
+    {
+        if (lives <= 0)
+        {
+            throw std::runtime_error("Out of lives! Game Over.");
+        }
+
+        if (n == number)
+        {
+            return true;
+        }
+        else
+        {
+            lives--;
+            return false;
+        }
+    }
+
+private:
+    int number;
+    int lives;
+};
