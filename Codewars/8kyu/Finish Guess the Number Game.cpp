e26a21dd7f85a120a3d97e047226b16908803aba
suankun <edmont@abv.bg>
2023-07-28 16:48:28 +0300

    add snippets

--- Codewars/8kyu/Finish Guess the Number Game.cpp
+++ Codewars/8kyu/Finish Guess the Number Game.cpp
@@ -31,3 +31,31 @@ private:
     int number;
     int lives;
 };
+//
+#include <stdexcept>
+
+class Guesser
+{
+public:
+    Guesser(int number, int lives)
+      : number(number), lives(lives)
+    { }
+    bool guess(int n)
+    {
+        if(lives<=0)
+        {
+          throw std::exception();
+        }
+        if(n==number)
+        {
+          return true;
+        }
+        else
+        {
+          lives--;
+          return false;
+        }
+    }
+private:
+    int number, lives;
+};
