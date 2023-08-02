28929d20650ddd5110c4b13f1c4927d91318b4eb
suankun <edmont@abv.bg>
2023-08-02 21:38:23 +0300

    add snippets

---
+++ Codewars/8kyu/Gravity Flip.cpp
@@ -0,0 +1,15 @@
+// https://www.codewars.com/kata/5f70c883e10f9e0001c89673/train/cpp
+#include <vector>
+#include <algorithm>
+
+std::vector<int> flip(const char dir, const std::vector<int>& arr) {
+    std::vector<int> result = arr;
+    if (dir == 'R') {
+        // Sort the vector in ascending order to move the smaller cubes to the right
+        std::sort(result.begin(), result.end());
+    } else if (dir == 'L') {
+        // Sort the vector in descending order to move the smaller cubes to the left
+        std::sort(result.rbegin(), result.rend());
+    }
+    return result;
+}
