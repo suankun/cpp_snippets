b29224e6716310617be55be1bd026f1b5a94acff
suankun <edmont@abv.bg>
2023-08-11 16:05:19 +0300

    add snippets

---
+++ Basics/09.OOP-ClassesAndObjects/defaultParametersForAConstructor/exercise/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "point.h"
+
+int main() {
+    Point p1;
+    std::cout << "x-value : " << p1.x() << std::endl;
+    std::cout << "y-value : " << p1.y() << std::endl;
+
+    return 0;
+}
