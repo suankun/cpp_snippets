823db0743fdd8df8eaa22451d60785eeea567529
suankun <edmont@abv.bg>
2023-08-23 22:09:31 +0300

    add snippets

--- Codewars/8kyu/Beginner Series #1 School Paperwork.cpp
+++ Codewars/8kyu/Beginner Series #1 School Paperwork.cpp
@@ -1,19 +1,12 @@
 // https://www.codewars.com/kata/55f9b48403f6b87a7c0000bd/train/csharp
-  using System;
-  public static class Paper
-  {
-    public static int Paperwork(int n, int m)
-    {
-      return n<0 ? 0 : m<0 ? 0 : n*m;
+int paperwork(int n, int m) {
+    if (n < 0 || m < 0) {
+        return 0;
     }
-  }
+    
+    return n * m;
+}
 //
-  using System;
-  public static class Paper
-  {
-    public static int Paperwork(int n, int m)
-    {
-      //#Happy Coding! ^_^
-      return (n < 0 | m < 0) ? 0 : n * m;
-    }
-  }
+int paperwork(int n, int m) {
+  return n < 0 || m < 0 ? 0 : n * m;
+}
