fb40474f1b3e918405df05c4c5da394cebb1740b
suankun <edmont@abv.bg>
2023-08-27 21:58:38 +0300

    add snippets

---
+++ Codewars/8kyu/Total amount of points.cpp
@@ -0,0 +1,25 @@
+// https://www.codewars.com/kata/5bb904724c47249b10000131/train/cpp
+#include <iostream>
+#include <array>
+using namespace std;
+
+int points(const std::array<std::string, 10>& games) {
+    int total_points {0};
+    for (size_t i {}; i<games.size(); i++) {
+        if (games[i][0] < games[i][2]) {
+            total_points += 0;
+        } else if (games[i][0] == games[i][2]) {
+            total_points += 1;
+        } else if (games[i][0] > games[i][2]) {
+            total_points += 3;
+        }
+    }
+    return total_points;
+}
+
+int main() {
+    std::array<std::string, 10> games = {"1:0","2:0","3:0","4:0","2:1","3:1","4:1","3:2","4:2","4:3"};
+    cout << points(games) << endl;  // 30
+
+    return 0;
+}
