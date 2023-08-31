e1d2139ada69ab69969ea4133161c8c5412e28ec
suankun <edmont@abv.bg>
2023-08-31 11:02:53 +0300

    add snippets

--- Codewars/8kyu/Floating point comparison.cpp
+++ Codewars/8kyu/Floating point comparison.cpp
@@ -6,3 +6,7 @@ bool approx_equals(double a, double b) {
     if (abs(a - b) >= 0.001) return false;
     return true;
 }
+//
+bool approx_equals(double a, double b) {
+  return abs(a - b) < 0.001;
+}
