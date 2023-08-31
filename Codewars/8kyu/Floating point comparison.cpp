92123dd447bd7a46679b1d17abeee09deede3888
suankun <edmont@abv.bg>
2023-08-31 11:02:10 +0300

    add snippets

---
+++ Codewars/8kyu/Floating point comparison.cpp
@@ -0,0 +1,8 @@
+// https://www.codewars.com/kata/5f9f43328a6bff002fa29eb8/train/cpp
+#include <cmath>
+using namespace std;
+
+bool approx_equals(double a, double b) {
+    if (abs(a - b) >= 0.001) return false;
+    return true;
+}
