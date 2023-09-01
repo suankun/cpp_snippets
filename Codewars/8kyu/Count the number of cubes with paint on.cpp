316f65a0e28029074d0b678f0cf028c68eddae4a
suankun <edmont@abv.bg>
2023-09-01 18:34:26 +0300

    add snippets

---
+++ Codewars/8kyu/Count the number of cubes with paint on.cpp
@@ -0,0 +1,17 @@
+// https://www.codewars.com/kata/5763bb0af716cad8fb000580/train/cpp
+int countSquares(int cuts) {
+    if (cuts == 0) {
+        return 1;
+    }
+    
+    int total_cubes = (cuts + 1) * (cuts + 1) * (cuts + 1);
+    int cubes_with_red_face = (cuts - 1) * (cuts - 1) * (cuts - 1);
+    
+    return total_cubes - cubes_with_red_face;
+}
+//
+int countSquares(int cuts)
+{
+    if (cuts == 0) return 1;
+    return 2*(3*cuts*cuts + 1);
+}
