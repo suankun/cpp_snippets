c05822da2d6fca8330e80bffe834b9d5c7dd17fd
suankun <edmont@abv.bg>
2023-09-05 15:02:13 +0300

    add snippets

---
+++ Codewars/7kyu/Determine the logarithm base.cpp
@@ -0,0 +1,14 @@
+// https://www.codewars.com/kata/5ae1dcde9c0e489ae00019fc/train/cpp
+#include <cmath>
+#include <functional>
+
+double DetermineBase(std::function<double(double)> log_function) {
+    return std::exp(std::log(2.0) / log_function(2.0));
+}
+//
+#include <functional>
+#include <cmath>
+
+double DetermineBase(std::function<double(double)> log_function) {
+    return exp(1.0 / log_function(M_E)); // log[e](base) = 1 / log[base](e)  --->  base = e ^ (1 / log[base](e))
+}
