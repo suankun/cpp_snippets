adfa0be7ddbee80e2787bcdb66af3ad901fe190e
suankun <edmont@abv.bg>
2023-09-07 21:33:16 +0300

    add snippets

---
+++ Codewars/7kyu/Tidy Number (Special Numbers Series #9).cpp
@@ -0,0 +1,24 @@
+// https://www.codewars.com/kata/5a87449ab1710171300000fd/train/cpp
+#include <string>
+
+using namespace std; 
+
+
+bool tidyNumber (int number)
+{
+  while(number>0) {
+    int temp1 = number%10;
+    number/=10;
+    int temp2 = number%10;
+    if (temp1<temp2) return false;
+  }
+  return true;
+}
+//
+#include <bits/stdc++.h>
+using namespace std; 
+
+bool tidyNumber (int number)
+{
+  string convNum = to_string(number); return is_sorted(convNum.begin(),convNum.end());
+}
