c9765959c1d91f45444b2b547dd5fef2e9a4fc2a
suankun <edmont@abv.bg>
2023-10-07 17:08:21 +0300

    add snippets

--- Codewars/7kyu/Next Prime.cpp
+++ Codewars/7kyu/Next Prime.cpp
@@ -25,3 +25,26 @@ int main() {
     std::cout << nextPrime(5) << std::endl;  // 7
     return 0;
 }
+//
+bool isPrime(int num)
+{
+    for (int i = 2; i * i <= num; i++)
+    {
+        if (num % i == 0)
+            return false;
+    }
+    return true;
+}
+
+int nextPrime(int num)
+{
+    if (num == 0 || num == 1)
+        return 2;
+    
+    for (int i = num + 1; ; i++)
+    {
+        if (isPrime(i))
+            return i;
+    }
+    return -1;
+}
