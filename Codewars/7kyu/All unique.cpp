a568cacf631b7cedfddd95d53c5782f373ac6f1d
suankun <edmont@abv.bg>
2023-10-08 12:36:35 +0300

    add snippets

---
+++ Codewars/7kyu/All unique.cpp
@@ -0,0 +1,11 @@
+// https://www.codewars.com/kata/553e8b195b853c6db4000048/train/cpp
+#include <iostream>
+bool hasUniqueChars(std::string s) {
+    if (s.length() == 0) return true;
+    for (size_t i {}; i < s.length()-1; i++) {
+        for (size_t j {i+1}; j < s.length(); j++) {
+            if (s[i] == s[j]) return false;
+        }
+    }
+    return true;
+}
