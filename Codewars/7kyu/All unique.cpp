da7cb40dd4aae2cd406d767955913aabc51df18d
suankun <edmont@abv.bg>
2023-10-08 12:37:22 +0300

    add snippets

--- Codewars/7kyu/All unique.cpp
+++ Codewars/7kyu/All unique.cpp
@@ -9,3 +9,9 @@ bool hasUniqueChars(std::string s) {
     }
     return true;
 }
+//
+#include <set>
+
+bool hasUniqueChars(std::string s) {
+  return s.size() == std::set<char>(s.begin(), s.end()).size();
+}
