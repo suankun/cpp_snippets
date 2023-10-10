6fcca9eabf233c06df4bef922f0a69ae5a17d5df
suankun <edmont@abv.bg>
2023-10-10 11:41:43 +0300

    add snippets

---
+++ Codewars/7kyu/Largest 5 digit number in a series.cpp
@@ -0,0 +1,20 @@
+// https://www.codewars.com/kata/51675d17e0c1bed195000001/train/cpp
+#include <iostream>
+#include <string>
+
+int largest_five_digits(const std::string &digits)
+{
+    int largest_num = 0;
+    for (size_t i=0; i<digits.length()-4; i++) {
+        int temp_num = stoi(digits.substr(i, 5));
+        if (largest_num < temp_num) largest_num = temp_num;
+    }
+    return largest_num;
+}
+
+int main() {
+    std::cout << largest_five_digits("283910") << std::endl;  // 83910
+    std::cout << largest_five_digits("1234567890") << std::endl;  // 67890
+
+    return 0;
+}
