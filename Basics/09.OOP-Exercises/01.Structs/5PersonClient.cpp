35c4cd3321f9b3f681e188e7378b6d03593906ba
suankun <edmont@abv.bg>
2023-10-23 20:05:11 +0300

    add snippets

--- Basics/09.OOP-Exercises/01.Structs/5PersonClient.cpp
+++ Basics/09.OOP-Exercises/01.Structs/5PersonClient.cpp
@@ -15,6 +15,10 @@ void read_person(Person& P) {
     cout << "Enter last name: \n"; cin >> P.l_name;
 }
 
+void print_person(const Person& P) {
+    cout << "First name: " << P.f_name << endl << "Last name: " << P.l_name << endl;
+}
+
 struct Client {
     Person name;
     double account;
@@ -25,12 +29,26 @@ void read_client(Client& C) {
     cout << "Account balance: "; cin >> C.account; 
 }
 
+void print_client(const Client& C) {
+    cout << "Name of the client: \n";
+    print_person(C.name);
+    cout << "Balance: " << C.account << endl;
+}
+
 int main() {
     cout << "Enter value for 'n' from 1 to " << MAX_SIZE << ": ";
     int n; cin >> n;
     Client a[MAX_SIZE];
-    for (int i=0; i<n; i++) {
+    int i;
+    for (i=0; i<n; i++) {
         read_client(a[i]);
+    }
+
+    // for (i=0; i<n; i++) {
+    //     cout << "Name: " << a[i].name.f_name << " " << a[i].name.l_name << ", balance: " << a->account << endl;
+    // }
+    for (i=0; i<n; i++) {
+        print_client(a[i]);
     }
 
     return 0;
