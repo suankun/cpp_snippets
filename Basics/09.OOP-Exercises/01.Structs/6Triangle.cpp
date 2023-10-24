3ee901050f659f7b7686baddd0c37981a83ce78a
suankun <edmont@abv.bg>
2023-10-24 21:49:07 +0300

    add snippets

--- Basics/09.OOP-Exercises/01.Structs/6Triangle.cpp
+++ Basics/09.OOP-Exercises/01.Structs/6Triangle.cpp
@@ -17,6 +17,11 @@ void show_point(const Point& p) {
     cout << "Point (" << p.x << ", " << p.y << ")";
 }
 
+double segment(const Point& p1, const Point& p2) {
+    return sqrt((p2.x-p1.x)*(p2.x-p1.x) +
+                (p2.y*p1.y)*(p2.y-p1.y));
+}
+
 struct Triangle {
     Point A, B, C;
 };
