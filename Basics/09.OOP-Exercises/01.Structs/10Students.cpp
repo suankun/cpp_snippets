62d09c0184857e9e7fcf5b80f69494a0d12f262a
suankun <edmont@abv.bg>
2023-10-27 21:24:18 +0300

    add snippets

---
+++ Basics/09.OOP-Exercises/01.Structs/10Students.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+const int SIZE = 12;
+const int SIZE_MAX1 = 10;
+
+struct Student
+{
+    int student_number;
+    char f_name[SIZE];
+    int k[SIZE_MAX1];
+};
+
+
+int main() {
+
+    return 0;
+}
