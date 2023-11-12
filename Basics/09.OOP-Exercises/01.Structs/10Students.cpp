c0576d0843ee9fbad2402c1e974b230f4aeb380b
suankun <edmont@abv.bg>
2023-11-12 21:21:09 +0200

    add snippets

--- Basics/09.OOP-Exercises/01.Structs/10Students.cpp
+++ Basics/09.OOP-Exercises/01.Structs/10Students.cpp
@@ -48,6 +48,84 @@ double result(const Student& s)
     return u;
 }
 
+void create_group(int n, Student *s)
+{
+    for(int i = 0; i < n; i++)
+    {
+        cout << "Data for " << i+1 << "-th student: ";
+        create_student(s[i]);
+    }
+}
+
+void print_table()
+{
+    cout << setiosflags(ios::fixed|ios::showpoint)
+         << setprecision(2);
+    int i;
+    for(i = 0; i < 5+SIZE+2+5*(k+1)+1; i++)
+        cout << "=";
+    cout << endl;
+    cout << setw(5) << "F.N."
+         << setw(SIZE+2) << "Family name";
+
+    for(i = 0; i < k; i++)
+        cout << setw(4) << "Dis-" << i+1;
+    cout << setw(6) << "Avr.gr." << endl;
+    for(i = 0; i < 5+SIZE+2+5*(k+1)+1; i++)
+        cout << "=";
+    cout << endl;
+}
+
+void print_group(int n, Student *s)
+{
+    print_table();
+    int i, j;
+
+    double temp[SIZE_MAX1] = {0, };
+    for(j = 0; j < k; j++)
+    {
+        for(i = 0; i < n; i++)
+            temp[j] = temp[j] + s[i].subjects[j];
+        temp[j] = temp[j]/n;
+    }
+
+    double sum = 0;
+    for(i = 0; i < n; i++)
+    {
+        print_student(s[i]);
+        double res = result(s[i]);
+        sum += res;
+        cout << setw(6) << res << endl;
+    }
+    for(i = 0; i < 5+SIZE+2+5*(k+1)+1; i++)
+        cout << "=";
+
+    cout << endl << setw(5+SIZE+2+5) << temp[0];
+    for(j = 1; j < k; j++)
+        cout << setw(5) << temp[j];
+    cout << setw(6) << sum/n << endl;
+}
+
+void sortgroup(int n, Student **s)
+{
+    for(int i = 0; i < n-1; i++)
+    {
+        int k = i;
+        double max = result(*s[k]);
+        for(int j = i+1; j < n; j++)
+        { 
+            double d = result(*s[j]);
+            if(d > max)
+            { 
+                max = d;
+                k = j;
+            }
+        }
+        Student* x;
+        x = s[i]; s[i] = s[k]; s[k] = x;
+    }
+}
+
 int main()
 {
     do
@@ -67,6 +145,17 @@ int main()
 
     Student s[SIZE_MAX2];
     Student* ps[SIZE_MAX2];
+
+    create_group(n, s);
+
+    print_group(n, s);
+    
+    for(int i = 0; i < n; i++)
+        ps[i] = &s[i];
+    
+    sortgroup(n, ps);
+
+    cout << endl;
 
     return 0;
 }
