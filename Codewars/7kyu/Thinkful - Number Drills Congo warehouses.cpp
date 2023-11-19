9ecb2c16bcebee48df634137b77716f9e2fa4e5f
suankun <edmont@abv.bg>
2023-11-19 21:55:13 +0200

    add snippets

---
+++ Codewars/7kyu/Thinkful - Number Drills Congo warehouses.cpp
@@ -0,0 +1,14 @@
+// https://www.codewars.com/kata/5862e7c63f8628a126000e18/train/cpp
+int box_capacity(int length, int width, int height) {
+  // calculate one pizza storage in feet, turn inces to feet
+  float pizza_storage = 16.0/12;
+  
+  // boxes in length
+  int len_boxes = length/pizza_storage;
+  // boxes in width
+  int wid_boxes = width/pizza_storage;
+  // boxes in height
+  int hei_boxes = height/pizza_storage;
+    
+  return len_boxes*wid_boxes*hei_boxes;
+}
