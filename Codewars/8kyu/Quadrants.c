67e685c6ea0a26ef00644960f79089dadc6eee98
suankun <edmont@abv.bg>
2023-11-20 21:19:42 +0200

    add snippets

---
+++ Codewars/8kyu/Quadrants.c
@@ -0,0 +1,14 @@
+// https://www.codewars.com/kata/643af0fa9fa6c406b47c5399/train/c
+enum quadrants {I=1, II, III, IV};
+
+enum quadrants quadrant(int x, int y) {
+    if (x > 0 && y > 0) {
+        return 1; // First quadrant
+    } else if (x < 0 && y > 0) {
+        return 2; // Second quadrant
+    } else if (x < 0 && y < 0) {
+        return 3; // Third quadrant
+    } else {
+        return 4; // Fourth quadrant
+    }
+}
