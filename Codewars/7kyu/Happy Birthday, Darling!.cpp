9987669bd4cde58af8d982013204b2d8527d9bcc
suankun <edmont@abv.bg>
2023-11-21 21:44:29 +0200

    add snippets

---
+++ Codewars/7kyu/Happy Birthday, Darling!.cpp
@@ -0,0 +1,20 @@
+// https://www.codewars.com/kata/5e96332d18ac870032eb735f/train/cpp
+#include <string>
+
+std::string womens_age(unsigned n) {
+  std::string sec_digit;
+  unsigned base;
+  for (unsigned i=n-2; i>=0; i--) {
+    if (n%i==0) {
+      sec_digit="0";
+      base = i;
+      break;
+    } else if (n%i==1) {
+      sec_digit="1";
+      base = i;
+      break;
+    }
+  }
+  std::string res = std::to_string(n) + "? That's just 2" + sec_digit + ", in base " + std::to_string(base) + "!";
+  return res;
+}
