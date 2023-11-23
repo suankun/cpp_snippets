633f05dcca941d76e9f7f93a777ed8ed9bc23c60
suankun <edmont@abv.bg>
2023-11-23 20:00:56 +0200

    add snippets

--- Codewars/7kyu/From A to Z.cpp
+++ Codewars/7kyu/From A to Z.cpp
@@ -10,3 +10,9 @@ std::string gimme_the_letters(const std::string& sp)
 
     return letters;
 }
+//
+std::string gimme_the_letters( const std::string& s ) {
+  std::string r{ s.front() };
+  while ( r.back() != s.back() ) r += r.back() + 1;
+  return r;
+}
