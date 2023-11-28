a49693b37dbc4d742ea07a15a95e18dcafe744cc
suankun <edmont@abv.bg>
2023-11-28 07:40:15 +0200

    add snippets

---
+++ Codewars/7kyu/Simple letter removal.cpp
@@ -0,0 +1,32 @@
+// https://www.codewars.com/kata/5b728f801db5cec7320000c7/train/cpp
+#include <iostream>
+using namespace std;
+
+std::string solve(const std::string &s, unsigned k)
+{
+    std::string result = s;
+
+    for (char c = 'a'; k > 0 && c <= 'z'; c++)
+    {
+        size_t found = result.find(c);
+
+        while (found != std::string::npos && k > 0)
+        {
+            result.erase(found, 1);
+            k--;
+            found = result.find(c);
+        }
+    }
+
+    return result;
+}
+
+int main() {
+    cout << solve("abracadabra", 1) << endl;  // 'bracadabra'
+    cout << solve("abracadabra", 2) << endl;  // 'brcadabra'
+    cout << solve("abracadabra", 6) << endl;  // 'rcdbr'
+    cout << solve("abracadabra", 8) << endl;  // 'rdr'
+    cout << solve("abracadabra", 50) << endl;  // ''
+
+    return 0;
+}
