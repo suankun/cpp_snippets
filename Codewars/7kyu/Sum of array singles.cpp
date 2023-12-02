e7c78f295cb5e66a718fef78fb53f8701094d84e
suankun <edmont@abv.bg>
2023-12-02 19:22:00 +0200

    add snippets

---
+++ Codewars/7kyu/Sum of array singles.cpp
@@ -0,0 +1,26 @@
+// https://www.codewars.com/kata/59f11118a5e129e591000134/train/cpp
+#include <iostream>
+using namespace std;
+
+#include <vector>
+#include <algorithm>
+
+int repeats(std::vector<int>v){
+    int sum=0;
+    sort(v.begin(), v.end());
+    for (size_t i=0; i<v.size(); i++) {
+        if (v[i]==v[i+1]) i++;
+        else sum+=v[i];
+    }
+
+    return sum;
+}
+
+int main() {
+    cout << repeats({4, 5, 7, 5, 4, 8}) << endl;  // 15
+    cout << repeats({9, 10, 19, 13, 19, 13}) << endl;  // 19
+    cout << repeats({16, 0, 11, 4, 8, 16, 0, 11}) << endl;  // 12
+    cout << repeats({5, 17, 18, 11, 13, 18, 11, 13}) << endl;  // 22
+
+    return 0;
+}
