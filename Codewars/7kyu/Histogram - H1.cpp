4e63eb40e4ba9e0f9a4b9885991b60d29a4f6858
suankun <edmont@abv.bg>
2023-12-08 20:41:40 +0200

    add snippets

---
+++ Codewars/7kyu/Histogram - H1.cpp
@@ -0,0 +1,42 @@
+// https://www.codewars.com/kata/57d532d2164a67cded0001c7/train/cpp
+#include <iostream>
+using namespace std;
+
+#include <vector>
+#include <string>
+
+std::string histogram(const std::vector<int>& results) {
+    std::string result;
+
+    for (int i = 6; i >= 1; i--) {
+        result += std::to_string(i) + '|';
+
+        // Append '#' for each count, or a space if count is 0
+        for (int j = 0; j < results[i - 1]; j++) {
+            result += '#';
+        }
+
+        // Append the count if it's greater than 0
+        if (results[i - 1] > 0) {
+            result += ' ' + std::to_string(results[i - 1]);
+        }
+
+        result += '\n';
+    }
+
+    return result;
+}
+
+int main() {
+    std::vector hist = {7, 3, 10, 1, 0, 5};
+
+    cout << histogram(hist) << endl;
+    // "6|##### 5\n"
+    // "5|\n"
+    // "4|# 1\n"
+    // "3|########## 10\n"
+    // "2|### 3\n"
+    // "1|####### 7\n";
+
+    return 0;
+}
