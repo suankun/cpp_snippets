657c82766ff2f737b9457028e8982c5d34ef6f62
suankun <edmont@abv.bg>
2023-12-09 07:08:14 +0200

    add snippets

---
+++ Codewars/7kyu/Number of measurements to spot the counterfeit coin.cpp
@@ -0,0 +1,34 @@
+// https://www.codewars.com/kata/59530d2401d6039f8600001f/train/cpp
+#include <iostream>
+using namespace std;
+
+#include <cmath>
+
+unsigned int howManyMeasurements(unsigned int n) {
+    // Base case: If there is only one coin, no weighings are needed.
+    if (n == 1) {
+        return 0;
+    }
+
+    // Calculate the logarithm base 3 of n to determine the number of levels in the binary search tree.
+    unsigned int levels = static_cast<unsigned int>(std::ceil(std::log2(n) / std::log2(3)));
+
+    // The minimum number of weighings is equal to the number of levels.
+    return levels;
+}
+
+int main() {
+    cout << howManyMeasurements(1) << endl;    // 0
+    cout << howManyMeasurements(2) << endl;    // 1
+    cout << howManyMeasurements(3) << endl;    // 1
+    cout << howManyMeasurements(8) << endl;    // 2
+    cout << howManyMeasurements(100) << endl;  // 5
+
+    return 0;
+}
+//
+#include <cmath>
+
+unsigned int howManyMeasurements(unsigned int n){
+  return ceil(log(n)/log(3));
+}
