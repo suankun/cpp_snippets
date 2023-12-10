bf2fd74d2f7be003a69ca4ce795feeb7433d1039
suankun <edmont@abv.bg>
2023-12-10 18:15:08 +0200

    add snippets

---
+++ Codewars/7kyu/Distance from the average.cpp
@@ -0,0 +1,30 @@
+// https://www.codewars.com/kata/568ff914fc7a40a18500005c/train/cpp
+#include <iostream>
+#include <vector>
+using namespace std;
+
+std::vector<double> distancesFromAverage(std::vector<int> input)
+{
+    std::vector<double> res;
+
+    // Find the average..
+    double sum=0.0;
+    for (auto n: input) sum+=n;
+    double avg=sum/input.size();
+
+    // Calc the differences and push them in the res vector.
+    for (auto n: input) {
+        double temp=avg-n;
+        res.push_back(temp);
+    }
+
+    return res;
+}
+
+int main() {
+    std::vector<double> dist = distancesFromAverage({55, 95, 62, 36, 48});  // 4.2, -35.8, -2.8, 23.2, 11.2, avg=59.2
+    for (auto n: dist) cout << n << ' ';
+    cout << endl;
+    
+    return 0;
+}
