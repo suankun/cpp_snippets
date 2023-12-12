c35ebe16e97bc770c592b793c28bbbaf91e92253
suankun <edmont@abv.bg>
2023-12-12 19:52:58 +0200

    add snippets

--- Codewars/7kyu/Extra Perfect Numbers (Special Numbers Series #7).cpp
+++ Codewars/7kyu/Extra Perfect Numbers (Special Numbers Series #7).cpp
@@ -8,3 +8,14 @@ vector <int> extraPerfect ( int number )
   for (int i=1; i<=number; i+=2) res.push_back(i);
   return res ;
 }
+//
+#include <vector>
+
+std::vector<int> extraPerfect(int n)
+{
+    std::vector<int> result;
+    result.reserve((n + 1) / 2);
+    for (int i = 1; i <= n; i += 2)
+        result.push_back(i);
+    return result;
+}
