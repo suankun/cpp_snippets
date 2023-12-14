278eec8b9d252fad90bf81026aedc15b64ac0874
suankun <edmont@abv.bg>
2023-12-14 21:29:17 +0200

    add snippets

---
+++ Codewars/7kyu/Basics 04 Rotate Matrix.cpp
@@ -0,0 +1,40 @@
+// https://www.codewars.com/kata/56b5dd1702a30326ce000b02/train/cpp
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+std::vector<std::vector<int>> rotateMatrixLeft(std::vector<std::vector<int>> matrix)
+{
+    int n=matrix[0].size();
+    int m=matrix.size();
+    std::vector<std::vector<int>> rotated(n, vector<int>(m, 0));
+
+    for (size_t i=0; i<rotated.size(); i++) {
+        for (size_t j=0; j<rotated[i].size(); j++) {
+            rotated[i][j]=matrix[j][i];
+        }
+    }
+    reverse(rotated.begin(), rotated.end());
+
+    return rotated;
+}
+
+int main() {
+    std::vector<std::vector<int>> input = {{-1, 4, 5},
+                                           { 2, 3, 4}};
+
+    std::vector<std::vector<int>> output = rotateMatrixLeft(input);
+    for (size_t i=0; i<output.size(); i++) {
+        for (size_t j=0; j<output[i].size(); j++) {
+            cout << output[i][j] << ' ';
+        }
+        cout << endl;
+    }
+
+    // Output: {{ 5, 4},
+    //          { 4, 3},
+    //          {-1, 2}}
+
+    return 0;
+}
