d9a62cc6a238a36c5d1653523c030ef74d433c97
suankun <edmont@abv.bg>
2023-12-15 19:48:18 +0200

    add snippets

--- Codewars/7kyu/Slaphead.cpp
+++ Codewars/7kyu/Slaphead.cpp
@@ -33,3 +33,10 @@ int main() {
 
     return 0;
 }
+//
+#include <algorithm>  // for count
+std::pair< std::string, std::string> bald( const std::string& s ) {
+    const long c{ count( s.cbegin(), s.cend(), '/' )};
+    return make_pair( std::string( s.size(), '-' ),
+        c > 5 ? "Hobo!" : c > 2 ? "Careless!" : c > 1 ? "Homer!" : c ? "Unicorn!" : "Clean!" );
+}
