f57127a2107be6fedd9daf20d0d6be4c0d35448b
suankun <edmont@abv.bg>
2023-12-17 18:09:35 +0200

    add snippets

--- Codewars/7kyu/Single character palindromes II.cpp
+++ Codewars/7kyu/Single character palindromes II.cpp
@@ -35,3 +35,11 @@ int main() {
 
     return 0;
 }
+//
+bool solve(std::string s)
+{
+	int count = 0, n = s.length();
+	for (int i = 0; i < n; i++) if (s[i] == s[n - i - 1]) count++;
+
+	return count == n && (n & 1) || count == n - 2;
+}
