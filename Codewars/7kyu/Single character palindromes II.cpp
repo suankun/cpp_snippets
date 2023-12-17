333a0c97b7be8c3d819b2be79e5f9ff2101503c2
suankun <edmont@abv.bg>
2023-12-17 18:04:55 +0200

    add snippets

---
+++ Codewars/7kyu/Single character palindromes II.cpp
@@ -0,0 +1,37 @@
+// https://www.codewars.com/kata/5a66ea69e6be38219f000110/train/cpp
+#include <iostream>
+#include <string>
+using namespace std;
+
+bool isPalindrome(std::string &s) {
+    for (size_t i=0; i<s.length()/2; i++) {
+        if (s[i]!=s[s.length()-i-1]) return false;
+    }
+    return true;
+}
+
+bool solve(std::string s){
+    if (isPalindrome(s) && s.length()%2==0) return false;
+    int cnt=0;
+    for (size_t i=0; i<s.length()/2; i++) {
+        if (s[i]!=s[s.length()-i-1]) {
+            cnt++;
+            if (cnt>1) return false;
+        }
+    }
+
+    return true;
+}
+
+int main() {
+    cout << boolalpha;
+    cout << solve("abba")  << endl;  // false
+    cout << solve("abbaa")  << endl;  // true
+    cout << solve("abbx")  << endl;  // true
+    cout << solve("aa")  << endl;  // false
+    cout << solve("ab")  << endl;  // true
+    cout << solve("abcba")  << endl;  // true
+    cout << solve("tdivcbtidgwrbkuxgisdyffydsigxukbrwgditbcvidtv")  << endl;  // false
+
+    return 0;
+}
