e6fc5132da2de51b3a554c6c6a505433e843f7be
suankun <edmont@abv.bg>
2023-12-18 19:18:31 +0200

    add snippets

--- LeetCode/1913. Maximum Product Difference Between Two Pairs.cpp
+++ LeetCode/1913. Maximum Product Difference Between Two Pairs.cpp
@@ -19,3 +19,29 @@ int main() {
 
     return 0;
 }
+//
+class Solution {
+public:
+    int maxProductDifference(vector<int>& nums) {
+        int largest = 0, secondLargest = 0;
+        int smallest = INT_MAX, secondSmallest = INT_MAX;
+
+        for (int n : nums) {
+            if (n < smallest) {
+                secondSmallest = smallest;
+                smallest = n;
+            } else if (n < secondSmallest) {
+                secondSmallest = n;
+            }
+
+            if (n > largest) {
+                secondLargest = largest;
+                largest = n;
+            } else if (n > secondLargest) {
+                secondLargest = n;
+            }
+        }
+
+        return (largest * secondLargest) - (smallest * secondSmallest);
+    }
+};
