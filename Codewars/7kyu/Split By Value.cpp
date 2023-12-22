efde995868263f09fff95b6668b917798213f50e
suankun <edmont@abv.bg>
2023-12-22 19:21:23 +0200

    add snippets

---
+++ Codewars/7kyu/Split By Value.cpp
@@ -0,0 +1,28 @@
+// https://www.codewars.com/kata/5a433c7a8f27f23bb00000dc/train/cpp
+#include <iostream>
+#include <vector>
+using namespace std;
+
+std::vector<int> split_by_value(int k, std::vector<int> elements)
+{
+    std::vector<int> res;
+    for (auto n: elements) if (n<k) res.push_back(n);
+    for (auto n: elements) if (n>=k) res.push_back(n);
+
+    return res;
+}
+
+int main() {
+    vector<int> res1 = split_by_value(5, {1, 3, 5, 7, 6, 4, 2});  // 1, 3, 4, 2, 5, 7, 6
+    vector<int> res2 = split_by_value(0, {5, 2, 7, 3, 2});  // 5, 2, 7, 3, 2
+    vector<int> res3 = split_by_value(6, {6, 4, 10, 10, 6});  // 4, 6, 10, 10, 6
+
+    for (auto n: res1) cout << n << ' ';
+    cout << endl;
+    for (auto n: res2) cout << n << ' ';
+    cout << endl;
+    for (auto n: res3) cout << n << ' ';
+    cout << endl;
+
+    return 0;
+}
