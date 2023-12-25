03975f3ca83d0d7d8fb01511487f4c0e4b3dfefa
suankun <edmont@abv.bg>
2023-12-25 16:50:41 +0200

    add snippets

---
+++ Codewars/7kyu/Principal Diagonal VS Secondary Diagonal.cpp
@@ -0,0 +1,27 @@
+// https://www.codewars.com/kata/5a8c1b06fd5777d4c00000dd/train/cpp
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+std::string diagonal (const std::vector<std::vector<int>> &matrix)
+{
+    int principal=0, secondary=0;
+    for (int i=0; i<matrix.size(); i++) {
+        for (int j=0; j<matrix[i].size(); j++) {
+            if (i==j) principal+=matrix[i][j];
+            if ((i+j)==(matrix.size()-1)) secondary+=matrix[i][j];
+        }
+    }
+
+    return principal>secondary ? "Principal Diagonal win!" : principal<secondary ? "Secondary Diagonal win!" : "Draw!";
+}
+
+int main() {
+    string res = diagonal({{2,2,2},
+                           {4,2,6},
+                           {8,8,2}});
+    cout << res << endl;
+
+    return 0;
+}
