0ea695ce32f043dbc33429ce617b1614f30271e6
suankun <edmont@abv.bg>
2023-12-26 20:43:23 +0200

    add snippets

--- LeetCode/27. Remove Element.cpp
+++ LeetCode/27. Remove Element.cpp
@@ -26,3 +26,17 @@ int main() {
 
     return 0;
 }
+//
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int index = 0;
+        for(int i = 0; i< nums.size(); i++){
+            if(nums[i] != val){
+                nums[index] = nums[i];
+                index++;
+            }
+        }
+        return index;
+    }
+};
