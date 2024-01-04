67365006c08b3543e04b8f07f76e5dc26b5c2d22
suankun <edmont@abv.bg>
2024-01-04 20:01:33 +0200

    add snippets

---
+++ SoftUni/1.PB/1. First Steps In Coding - Lab/04. Inches to Centimeters.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main() {
+    double num=0;
+    cin >> num;
+
+    cout << num*2.54 << endl;
+    
+    return 0;
+}
---
+++ SoftUni/1.PB/1. First Steps In Coding - Lab/05. Greeting by Name.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+
+int main() {
+    string name;
+    cin >> name;
+
+    cout << "Hello, " << name << "!" << endl;
+    
+    return 0;
+}
