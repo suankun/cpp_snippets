8a56091aaf20053e1829e9f41b272f63b6536ddf
suankun <edmont@abv.bg>
2024-01-08 19:25:42 +0200

    add snippets

---
+++ SoftUni/1. PB/4. For Loop - Lab/02. Numbers N...1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main() {
+    int n;
+    cin >> n;
+    for (int i=n; i>=1; i--) {
+        cout << i << endl;
+    }
+
+    return 0;
+}
