4d4357d6c3ee46f97068d332d5b0c5c75b00afe3
suankun <edmont@abv.bg>
2024-01-14 22:37:51 +0200

    add snipppets

--- Codewars/7kyu/Birthday I - Cake.cpp
+++ Codewars/7kyu/Birthday I - Cake.cpp
@@ -1,3 +1,4 @@
+// https://www.codewars.com/kata/5805ed25c2799821cb000005/train/cpp
 #include <iostream>
 #include <string>
 using namespace std;
@@ -5,8 +6,8 @@ using namespace std;
 std::string cake(int x, std::string y){
     int sum {};
     for (size_t i {}; i<y.length(); i++) {
-        if (i%2==0) sum += y[i];
-        else sum+=(y[i] - 'a' + 1);
+        if (i%2==0) sum+=y[i];
+        else sum+=(y[i] - 'a'+1);
     }
     return sum>=(x*0.7) ? "Fire!" : "That was close!";
 }
@@ -19,4 +20,10 @@ int main() {
     cout << cake(404, "eukgdm") << endl;  // "Fire!"
 
     return 0;
+}
+//
+std::string cake(int x, std::string y){
+    float sum=0;
+    for (unsigned int i = 0; i < y.size(); i++) sum += int(y[i]) - (i % 2 ? 96 : 0);
+    return sum/x < 0.7 ? "That was close!" : "Fire!";
 }
