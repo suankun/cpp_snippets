e7e593314debdc0ecd16942a2f5b40b56169d4bc
suankun <edmont@abv.bg>
2024-01-18 20:04:14 +0200

    add snipppets

---
+++ Codewars/7kyu/Convert Time to String.cpp
@@ -0,0 +1,16 @@
+// https://www.codewars.com/kata/5502ddd734137e90af000f62/train/cpp
+#include <string>
+
+std::string convertTime(int timeDiff)
+{
+    int days = timeDiff/86400;
+    timeDiff-=(days*86400);
+    int hours = timeDiff/3600;
+    timeDiff-=(hours*3600);
+    int minutes = timeDiff/60;
+    int seconds = timeDiff%60;
+    return std::to_string(days) + " " +
+      std::to_string(hours) + " " +
+      std::to_string(minutes) + " " +
+      std::to_string(seconds);
+}
