287aa4548137c5f50242cbfa219db2a034870821
suankun <edmont@abv.bg>
2024-01-19 23:05:50 +0200

    add snippets

---
+++ Codewars/7kyu/Oh come on! Hello world! once again.cpp
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/594a7ea087a7c6cbe60000d6/train/cpp
+#include <iostream>
+#include <string>
+std::string f() 
+{ 
+    return "\x48\x65\x6C\x6C\x6F\x2C\x20\x77\x6F\x72\x6C\x64\x21";
+}
