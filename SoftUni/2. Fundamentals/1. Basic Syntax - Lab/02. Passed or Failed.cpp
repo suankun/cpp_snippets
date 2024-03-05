948c8504304ead05dd6d2786381b8f672d39dc66
suankun <edmont@abv.bg>
2024-03-05 23:22:53 +0200

    add snippets

---
+++ SoftUni/2. Fundamentals/1. Basic Syntax - Lab/02. Passed or Failed.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    float grade;
+    cin >> grade;
+
+    if (grade >= 3.00) cout << "Passed!" << endl;
+    else cout << "Failed!" << endl;
+
+    return 0;
+}
