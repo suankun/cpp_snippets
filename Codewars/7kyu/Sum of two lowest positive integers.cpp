0ec9c6f49f3ef88e7d30ff872597c6628225dede
suankun <edmont@abv.bg>
2024-03-11 07:44:16 +0200

    add snippets

--- Codewars/7kyu/Sum of two lowest positive integers.cpp
+++ Codewars/7kyu/Sum of two lowest positive integers.cpp
@@ -16,3 +16,28 @@ int main() {
 
     return 0;
 }
+//
+#include <climits>
+
+using namespace std;
+
+long sumTwoSmallestNumbers(std::vector<int> numbers)
+{
+    long min1 = std::min(numbers[0],numbers[1]);
+    long min2 = std::max(numbers[0],numbers[1]);
+    
+    for (int i=2; i<numbers.size(); ++i)
+    {
+      if (numbers[i] <= min1)
+      {
+        min2 = min1;
+        min1 = numbers[i];
+      }
+      else if (numbers[i] > min1 && numbers[i] <= min2)
+      {
+        min2 = numbers[i];
+      }
+    }
+    
+    return min1 + min2;
+}
