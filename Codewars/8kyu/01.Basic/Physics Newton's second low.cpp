08018dd3d84712b0daca763c3991fbe617c81c76
suankun <edmont@abv.bg>
2024-03-17 11:22:06 +0200

    add snippets

---
+++ Codewars/8kyu/01.Basic/Physics Newton's second low.cpp
@@ -0,0 +1,12 @@
+// https://www.codewars.com/kata/65f619cd49a81c25bd237410/train/cpp
+double find_force(double m, double a) {
+    return m * a;
+}
+
+double find_mass(double F, double a) {
+    return F / a;
+}
+
+double find_acceleration(double F, double m) {
+    return F / m;
+}
