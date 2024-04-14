d23c7e5bc4b2de2fccde00859799d941b915942d
suankun <edmont@abv.bg>
2024-04-14 20:35:54 +0300

    add snippets

--- Codewars/7kyu/Rock Off!.cpp
+++ Codewars/7kyu/Rock Off!.cpp
@@ -28,3 +28,14 @@ int main() {
 
     return 0;
 }
+//
+using namespace std;
+string solve_rock_off(const vector<int>& alice, const vector<int>& bob) {
+    const vector<string> q{"Alice made \"Kurt\" proud!", "Bob made \"Jeff\" proud!",
+                                "that looks like a \"draw\"! Rock on!"};
+    int a = 0, b = 0;
+    for (size_t i = 0; i < bob.size(); i++)
+        if (alice[i] > bob[i]) a++;
+        else if (alice[i] < bob[i]) b++;
+    return to_string(a) + ", " + to_string(b) + ": " + (a == b ? q.back() : q[a<b]);
+}
