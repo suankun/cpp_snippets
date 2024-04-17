c4ab97171ccf12f06268e57fb7c666f158a81c0e
suankun <edmont@abv.bg>
2024-04-17 20:34:12 +0300

    add snippets

--- Codewars/8kyu/07.Vector/Flick Switch.cpp
+++ Codewars/8kyu/07.Vector/Flick Switch.cpp
@@ -14,3 +14,19 @@ std::vector<bool> flick_switch(const std::vector<std::string>& arr)
     }
     return result;
 }
+//
+#include <vector>
+#include <string>
+using namespace std ;
+vector<bool> flick_switch(const vector<string>& arr)
+{
+    vector<bool> flick ;
+    bool flic = true ;
+    for (string str : arr) {
+        if (str == "flick")
+            flic = !flic ;
+        flick.push_back(flic);
+    }
+  
+    return flick;
+}
