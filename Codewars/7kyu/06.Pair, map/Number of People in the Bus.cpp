a1397ae4b74f104a3f43e47e7df64c189fad53f3
suankun <edmont@abv.bg>
2024-04-19 07:40:50 +0300

    add snippets

--- Codewars/7kyu/06.Pair, map/Number of People in the Bus.cpp
+++ Codewars/7kyu/06.Pair, map/Number of People in the Bus.cpp
@@ -21,3 +21,11 @@ int main() {
 
     return 0;
 }
+//
+#include<vector>
+
+unsigned int number(const std::vector<std::pair<int, int>>& busStops){
+    int passengers = 0;
+    for(auto i: busStops)  passengers += i.first - i.second;
+    return passengers;
+}
