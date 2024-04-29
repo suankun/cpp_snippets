58c11d9e588b926c18f631ad2be7c6b093fcdd40
suankun <edmont@abv.bg>
2024-04-29 19:55:07 +0300

    add snippets

---
+++ Basics/08.Pointers/challange3.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+// print the element which is offset step after the beginnig of the array
+void shoot_forward( int data[], unsigned int offset){
+    
+    //Don't modify anything above this line
+    //Your code will go below this line
+    std::cout << "The element " << offset << " slots away from the beginning is : " << data[offset];
+
+    //Your code will go above this line
+    //Don't modify anything below this line
+}
