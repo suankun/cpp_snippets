7d239b0bc18f8ec772aed9944c6241fb3ec85566
suankun <edmont@abv.bg>
2024-05-08 23:25:46 +0300

    add snippets

---
+++ Codewars/7kyu/05.String/Char Code Calculation.cpp
@@ -0,0 +1,30 @@
+/// https://www.codewars.com/kata/57f75cc397d62fc93d000059/train/cpp
+#include <string>
+#include <cmath>
+
+int calc(const std::string& x) {
+    std::string replaced_seven = x;
+
+    std::string total1 = "";
+    std::string total2 = "";
+
+    for (char c : x) {
+        int temp = c;
+        total1 += std::to_string(temp);
+    }
+
+    for (char c : total1) {
+        char temp = (c == '7') ? '1' : c;
+        total2 += temp;
+    }
+  
+    int num1 = 0, num2 = 0;
+    for (size_t i = 0; i < total1.length(); i++) {
+      num1 += (10 * i) + (total1[i] - '0');
+    }
+    for (size_t i = 0; i < total2.length(); i++) {
+      num2 += (10 * i) + (total2[i] - '0');
+    }
+  
+    return std::abs(num1 - num2);
+}
