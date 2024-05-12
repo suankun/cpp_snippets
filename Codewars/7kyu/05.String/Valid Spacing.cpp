3db78ea2337883a3bfe7c8f9435fd7080d6c0b97
suankun <edmont@abv.bg>
2024-05-12 21:40:58 +0300

    add snippets

---
+++ Codewars/7kyu/05.String/Valid Spacing.cpp
@@ -0,0 +1,26 @@
+// https://www.codewars.com/kata/5f77d62851f6bc0033616bd8/train/cpp
+#include <iostream>
+#include <string>
+using namespace std;
+
+bool valid_spacing(const std::string& s) {
+    if (s[0] == ' ' || s[s.length()-1] == ' ') return false;
+    bool isSpace = false;
+    for (size_t i = 0; i < s.length(); i++) {
+        if (s[i] == ' ' && isSpace == true) return false;
+        else if (s[i] == ' ') isSpace = true;
+        else isSpace = false;
+    }
+    return true;
+}
+
+int main() {
+    cout << boolalpha;
+    cout << valid_spacing("Hello world") << endl;  // true
+    cout << valid_spacing(" Hello world") << endl;  // false
+    cout << valid_spacing("Hello  world ") << endl;  // false
+    cout << valid_spacing("Hello") << endl;  // true
+    cout << valid_spacing("Helloworld") << endl;  // true
+
+    return 0;
+}
