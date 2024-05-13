c6af590d26be3a3d830ba3c415e6475e468b705d
suankun <edmont@abv.bg>
2024-05-13 21:43:01 +0300

    add snippets

--- Codewars/7kyu/01.Basic, conditional, loops/Geometry Basics Dot Product in 3D.cpp
+++ Codewars/7kyu/01.Basic, conditional, loops/Geometry Basics Dot Product in 3D.cpp
@@ -2,6 +2,15 @@
 #include <cmath>   // for round()
 #include <iomanip> // for setprecision()
 
+// struct Vector {
+//     double x, y, z;
+//     Vector(double _x, double _y, double _z) : x(_x), y(_y), z(_z) {}
+// };
+
 double dot_product(const Vector& a, const Vector& b) {
     return round((a.x * b.x + a.y * b.y + a.z * b.z) * 1e6) / 1e6; // rounding to 6 decimal places
+}
+//
+double dot_product(const Vector& a, const Vector& b){
+    return a.x * b.x + a.y * b.y + a.z * b.z;
 }
