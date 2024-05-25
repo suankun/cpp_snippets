347de081486653683f6be305d409bdd5dccc0058
Edmont Budakyan <edmont@abv.bg>
2024-05-25 21:33:58 +0300

    add snippets

--- LeetCode/28. Find the Index of the First Occurrence in a String.cpp
+++ LeetCode/28. Find the Index of the First Occurrence in a String.cpp
@@ -6,13 +6,11 @@ using namespace std;
 class Solution {
 public:
     int strStr(string haystack, string needle) {
-        int needleSize = needle.length();
-        for (size_t i = 0; i < haystack.length() - needleSize; i++) {
-            string check = haystack.substr(i, needleSize);
-            if (check == needle)
-                return i;
-        }
-        return -1;
+        bool check = haystack.find(needle) != string::npos;
+        int idx = -1;
+        if (check)
+            idx = haystack.find(needle);
+        return idx;
     }
 };
 
@@ -23,3 +21,14 @@ int main() {
 
     return 0;
 }
+//
+class Solution {
+public:
+    int strStr(string s, string t) {
+        if(s.size()<t.size()) return -1;
+        for(int i=0;i<=s.size()-t.size();i++){
+            if(s.substr(i,t.size()) == t) return i;
+        }
+        return -1;
+    }
+};
