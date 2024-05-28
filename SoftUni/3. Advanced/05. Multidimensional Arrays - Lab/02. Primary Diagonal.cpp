f8fe4b329d7190b23580d9b9c1055a230fce44ef
Edmont Budakyan <edmont@abv.bg>
2024-05-28 20:41:24 +0300

    add snippets

---
+++ SoftUni/3. Advanced/05. Multidimensional Arrays - Lab/02. Primary Diagonal.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <array>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int matrix[n][n];
+
+    for (int rows = 0; rows < n; rows++) {
+        for (int cols = 0; cols < n; cols++) {
+            cin >> matrix[rows][cols];
+        }
+    }
+
+    int sum = 0;
+    for (int rows = 0; rows < n; rows++) {
+        for (int cols = 0; cols < n; cols++) {
+            if (rows == cols)
+                sum += matrix[rows][cols];
+        }
+    }
+
+    cout << sum << endl;
+
+    return 0;
+}
