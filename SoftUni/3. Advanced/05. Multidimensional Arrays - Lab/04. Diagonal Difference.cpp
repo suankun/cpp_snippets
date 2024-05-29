3c64b1a4d3ba5473909ff46cc16c0590de5f0149
Edmont Budakyan <edmont@abv.bg>
2024-05-29 16:19:18 +0300

    add snippets

---
+++ SoftUni/3. Advanced/05. Multidimensional Arrays - Lab/04. Diagonal Difference.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int matrix[n][n];
+
+    for (int rows = 0; rows < n; rows++) {
+        for (int cols = 0; cols < n; cols++) {
+            cin >> matrix[rows][cols];
+        }
+    }
+
+    int sumPrimaryDiag = 0, sumSecondDiag = 0;
+    for (int currIndex = 0; currIndex < n; currIndex++) {
+        sumPrimaryDiag += matrix[currIndex][currIndex];
+        sumSecondDiag += matrix[currIndex][n - 1 - currIndex];
+    }
+
+    cout << abs(sumPrimaryDiag - sumSecondDiag) << endl;
+
+    return 0;
+}
