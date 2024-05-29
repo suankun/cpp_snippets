530745b8e195c311df1b08f25f608fa164d49b8c
Edmont Budakyan <edmont@abv.bg>
2024-05-29 16:22:43 +0300

    add snippets

--- SoftUni/3. Advanced/05. Multidimensional Arrays - Lab/04. Diagonal Difference.cpp
+++ SoftUni/3. Advanced/05. Multidimensional Arrays - Lab/04. Diagonal Difference.cpp
@@ -16,9 +16,18 @@ int main() {
     }
 
     int sumPrimaryDiag = 0, sumSecondDiag = 0;
-    for (int currIndex = 0; currIndex < n; currIndex++) {
-        sumPrimaryDiag += matrix[currIndex][currIndex];
-        sumSecondDiag += matrix[currIndex][n - 1 - currIndex];
+    // for (int currIndex = 0; currIndex < n; currIndex++) {
+    //     sumPrimaryDiag += matrix[currIndex][currIndex];
+    //     sumSecondDiag += matrix[currIndex][n - 1 - currIndex];
+    // }
+
+    for (int rows = 0; rows < n; rows++) {
+        for (int cols = 0; cols < n; cols++) {
+            if (rows == cols)
+                sumPrimaryDiag += matrix[rows][cols];
+            if (rows + cols == n - 1)
+                sumSecondDiag += matrix[rows][cols];
+        }
     }
 
     cout << abs(sumPrimaryDiag - sumSecondDiag) << endl;
