863a21d868b7da6bc29c18bfada6fb1477e56b1c
Edmont Budakyan <edmont@abv.bg>
2024-06-10 12:27:25 +0300

    add snippets

--- Codewars/7kyu/Hands Up.cpp
+++ Codewars/7kyu/Hands Up.cpp
@@ -9,3 +9,11 @@ persons_t get_positions(int n) {
     int p3 = (n / 9) % 3;
     return { p1, p2, p3 };
 }
+//
+#include <array>
+
+using persons_t = std::array<int, 3>;
+
+persons_t get_positions(int n) {
+    return {n % 3, (n % 9) / 3, (n % 27) / 9};
+}
