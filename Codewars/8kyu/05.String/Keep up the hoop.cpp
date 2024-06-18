95b554f3adf37cb46f967dbb858a559f646d7410
Edmont Budakyan <edmont@abv.bg>
2024-06-18 20:44:09 +0300

    add snippets

---
+++ Codewars/8kyu/05.String/Keep up the hoop.cpp
@@ -0,0 +1,6 @@
+// https://www.codewars.com/kata/55cb632c1a5d7b3ad0000145/train/cpp
+#include <string>
+
+std::string hoop_count(unsigned n) {
+    return n >= 10 ? "Great, now move on to tricks" : "Keep at it until you get it";
+}
