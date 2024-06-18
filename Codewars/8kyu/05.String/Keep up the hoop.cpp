2fdc96a7af3297da3084ba1481c3fb9081be16ad
Edmont Budakyan <edmont@abv.bg>
2024-06-18 20:50:08 +0300

    add snippets

--- Codewars/8kyu/05.String/Keep up the hoop.cpp
+++ Codewars/8kyu/05.String/Keep up the hoop.cpp
@@ -4,3 +4,15 @@
 std::string hoop_count(unsigned n) {
     return n >= 10 ? "Great, now move on to tricks" : "Keep at it until you get it";
 }
+//
+#include <string>
+#include <array>
+
+inline constexpr unsigned gk_treshold = 10;
+inline const std::array<std::string, 2> gk_message = {
+    "Great, now move on to tricks", "Keep at it until you get it"
+};
+
+std::string hoop_count(unsigned n) {
+    return gk_message[n < gk_treshold];
+}
