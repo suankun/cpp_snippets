0d1517646cce54a864902828689f7763adc40e1a
Edmont Budakyan <edmont@abv.bg>
2024-06-19 15:21:25 +0300

    add snippets

--- Codewars/7kyu/05.String/Composing squared strings.cpp
+++ Codewars/7kyu/05.String/Composing squared strings.cpp
@@ -55,3 +55,38 @@ int main()
 
     return 0;
 }
+//
+#include <string>
+#include <vector>
+#include <sstream>
+#include <algorithm>
+
+class ComposeSqStrings
+{
+private:
+    static std::vector<std::string> split(const std::string &s);
+public:
+    static std::string compose(const std::string &s1, const std::string &s2);
+};
+
+std::vector<std::string> ComposeSqStrings::split(const std::string &s)
+{
+    std::vector<std::string> out;
+    std::istringstream ss(s);
+    std::string next;
+    while (std::getline(ss, next, '\n'))
+        out.push_back(next);
+    return out;
+}
+
+std::string ComposeSqStrings::compose(const std::string &s1, const std::string &s2)
+{
+    std::vector<std::string> arr2 = split(s2);
+    unsigned int l = arr2.size();
+    std::reverse(std::begin(arr2), std::end(arr2));
+    std::vector<std::string> arr1 = split(s1);
+    std::string res = "";
+    for (unsigned int i = 0; i < l; i++)
+        res += arr1[i].substr(0, i+1) + arr2[i].substr(0, l-i) + "\n";
+    return res.substr(0,  res.length()-1);
+}
