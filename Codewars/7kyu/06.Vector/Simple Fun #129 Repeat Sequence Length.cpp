d7cf52a0093e98ddd9c3d1db25888ebd37254f82
Edmont Budakyan <edmont@abv.bg>
2024-06-20 10:37:06 +0300

    add snippets

--- Codewars/7kyu/06.Vector/Simple Fun #129 Repeat Sequence Length.cpp
+++ Codewars/7kyu/06.Vector/Simple Fun #129 Repeat Sequence Length.cpp
@@ -39,3 +39,14 @@ int repeat_sequence_len(int a0) {
 
     return cycle_length;
 }
+//
+#include <cmath>
+int repeat_sequence_len( int a ) {
+    do { int sum{};
+        do sum += pow( a % 10, 2 );
+        while ( a /= 10 );
+        if ( sum == 1 ) return 1;
+        a = sum;
+    } while ( a > 9 );
+    return 8;
+}
