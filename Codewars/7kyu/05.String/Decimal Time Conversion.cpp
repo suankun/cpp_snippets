0294eaeb75c90356bd11431c94a91100851882b9
Edmont Budakyan <edmont@abv.bg>
2024-06-22 22:07:02 +0300

    add snippets

--- Codewars/7kyu/05.String/Decimal Time Conversion.cpp
+++ Codewars/7kyu/05.String/Decimal Time Conversion.cpp
@@ -72,3 +72,17 @@ std::string toNormal(double time){
     int m = round((time - h) * 60);
     return std::to_string(h) + ":" + std::to_string(m);
 }
+//
+#include <cmath>
+#include <fmt/format.h>
+double toIndustrial( int time ) {
+    return round( time / 60. * 100 ) / 100;
+}
+double toIndustrial( std::string time ) {
+    return toIndustrial( stoi( time ) * 60 +
+        stoi( time.substr( time.find( ':' ) + 1 )));
+}
+std::string toNormal( double time ) {
+    const int t = round( time * 60 );
+    return fmt::format( "{}:{:02}", t / 60, t % 60 );
+}
