bfa23d3d5a01146ef27f4b428add3d9e4e93fd27
Edmont Budakyan <edmont@abv.bg>
2024-06-23 07:02:42 +0300

    add snippets

--- Codewars/5kyu/Greed is Good.cpp
+++ Codewars/5kyu/Greed is Good.cpp
@@ -80,3 +80,15 @@ int main()
 
     return 0;
 }
+//
+#include <vector>
+
+int score(const std::vector<int>& dice) {
+    unsigned cs[7] = {}; // 7 because of the 0 element
+    for (auto d : dice)
+        cs[d]++;
+    return
+        cs[1] / 3 * 1000 + cs[6] / 3 * 600 + cs[5] / 3 * 500 +
+        cs[4] / 3 * 400 + cs[3] / 3 * 300 + cs[2] / 3 * 200 +
+        cs[1] % 3 * 100 + cs[5] % 3 * 50;
+}
