1b37638ca15ec5c449dcb7a78671cb1d0d8bacf1
Edmont Budakyan <edmont@abv.bg>
2024-06-27 15:37:57 +0300

    add snippets

--- Codewars/7kyu/09.OOP/Find heavy ball - level novice.cpp
+++ Codewars/7kyu/09.OOP/Find heavy ball - level novice.cpp
@@ -1,6 +1,12 @@
 // https://www.codewars.com/kata/544047f0cf362503e000036e/train/cpp
 #include<vector>
 
+struct Scales{
+    int getWeight(std::vector<int> left, std::vector<int> right) {
+        return left > right ? -1 : left < right ? 1 : 0;
+    }
+};
+
 unsigned int findBall(Scales scales) {
 // call scales.getWeight() max 4 times
 // return indexOfHeavyBall;
@@ -19,22 +25,32 @@ unsigned int findBall(Scales scales) {
   }
 }
 //
-#include<vector>
+// #include<vector>
 
-unsigned int findBall(Scales scales) {
-// call scales.getWeight() max 4 times
-// return indexOfHeavyBall;
-  for (unsigned int i = 1; i < 8; i+=2) {
-        std::vector<int> leftPan{i - 1};
-        std::vector<int> rightPan{i};
-        int w = scales.getWeight(leftPan, rightPan);
+// unsigned int findBall(Scales scales) {
+// // call scales.getWeight() max 4 times
+// // return indexOfHeavyBall;
+//     for (unsigned int i = 1; i < 8; i+=2) {
+//         std::vector<int> leftPan{i - 1};
+//         std::vector<int> rightPan{i};
+//         int w = scales.getWeight(leftPan, rightPan);
 
-        if (w == -1) {// left pan is heavier
-            return leftPan[0];
-        }
+//         if (w == -1) {// left pan is heavier
+//             return leftPan[0];
+//         }
 
-        if (w == 1) {// right pan is heavier
-            return rightPan[0];
-        }
-    }
-}
+//         if (w == 1) {// right pan is heavier
+//             return rightPan[0];
+//         }
+//     }
+// }
+// //
+// #include<vector>
+
+// int findBall(Scales scales) {
+//     for (int i = 0; i < 7; i += 2) {
+//         int w = scales.getWeight({ i }, { i + 1 });
+//         if (w == -1) return i;
+//         if (w == 1) return i + 1;
+//     }
+// }
