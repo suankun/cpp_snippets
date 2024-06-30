326a4e26babc7b86b9e49b0c0a514322ac826b23
Edmont Budakyan <edmont@abv.bg>
2024-06-30 07:48:33 +0300

    add snippets

---
+++ Codewars/7kyu/08.Pair, map, tuple/Cogs 2.cpp
@@ -0,0 +1,13 @@
+// https://www.codewars.com/kata/59e72bdcfc3c4974190000d9/train/cpp
+#include <vector>
+#include <utility>
+
+using namespace std;
+
+pair<double, double> cog_rpm(const vector<int> &cogs, int n)
+{
+    vector<int> d = {1, -1};
+    double first_value = d[n % 2] * static_cast<double>(cogs[n]) / cogs[0];
+    double second_value = d[(cogs.size() - n + 1) % 2] * static_cast<double>(cogs[n]) / cogs[cogs.size() - 1];
+    return {first_value, second_value};
+}
