b9a19fc75dc622cfb52eff81fdf003296abec0c0
Edmont Budakyan <edmont@abv.bg>
2024-07-07 22:45:04 +0300

    add snippets

---
+++ Codewars/7kyu/05.String/Simple fibonacci strings.cpp
@@ -0,0 +1,19 @@
+// https://www.codewars.com/kata/5aa39ba75084d7cf45000008/train/cpp
+#include <string>
+
+std::string solve(int n) {
+    if (n == 0) return "0";
+    if (n == 1) return "01";
+    
+    std::string f0 = "0";
+    std::string f1 = "01";
+    std::string fn;
+    
+    for (int i = 2; i <= n; i++) {
+        fn = f1 + f0;
+        f0 = f1;
+        f1 = fn;
+    }
+    
+    return fn;
+}
