0c2df6cef3ef54d1a8b26ec516e1163d4de61636
Edmont Budakyan <edmont@abv.bg>
2024-07-09 22:34:53 +0300

    add snippets

---
+++ Codewars/7kyu/06.Vector/Leonardo numbers.cpp
@@ -0,0 +1,13 @@
+// https://www.codewars.com/kata/5b2117eea454c89d4400005f/train/cpp
+#include <vector>
+
+std::vector<int> L (int n, int L0, int L1, int add) {
+    std::vector<int> res;
+    res.push_back(L0);
+    res.push_back(L1);
+    for (int i = 2; i < n; i++) {
+        int next = res[i-1] + res[i-2] + add;
+        res.push_back(next);
+    }
+    return res;
+}
