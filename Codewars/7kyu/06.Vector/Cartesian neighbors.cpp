03baf96c0d35b1ecf1e610bdb9525f5f2d4ff0aa
Edmont Budakyan <edmont@abv.bg>
2024-07-14 22:03:44 +0300

    add snippets

--- Codewars/7kyu/06.Vector/Cartesian neighbors.cpp
+++ Codewars/7kyu/06.Vector/Cartesian neighbors.cpp
@@ -8,3 +8,23 @@ std::vector<std::vector<int>> cartesianNeighbor(int x, int y) {
         {x+1, y-1}, {x+1, y}, {x+1, y+1}
     };
 }
+//
+#include <vector>
+using std::vector;
+
+vector< vector<int> > cartesianNeighbor(int x, int y){
+	vector< vector<int> > res;
+
+	for (int i : {0, -1, 1})
+	{
+		for (int j : {0, -1, 1})
+		{
+			if ((i == 0) && (j == 0))
+				continue;
+
+			res.push_back({x+i, y+j});
+		}
+	}
+
+	return res;
+}
