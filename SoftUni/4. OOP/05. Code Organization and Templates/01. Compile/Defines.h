8712cac71a9558d8450f8a52beb0613f6cea9a5e
Edmont Budakyan <edmont@abv.bg>
2024-07-16 11:40:52 +0300

    add snippets

---
+++ SoftUni/4. OOP/05. Code Organization and Templates/01. Compile/Defines.h
@@ -0,0 +1,11 @@
+#ifndef _DEFINES_H_
+#define _DEFINES_H_
+
+#include <iostream>
+#include <string>
+
+#define DONT_COMPILE_THIS
+
+#define STANDARD_TEMPLATE_LIBRARY namespace std
+
+#endif // _DEFINES_H_
