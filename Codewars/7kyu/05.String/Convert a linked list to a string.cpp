b12a733912e79748e252b61977cfd687344906f3
Edmont Budakyan <edmont@abv.bg>
2024-07-18 21:41:12 +0300

    add snippets

---
+++ Codewars/7kyu/05.String/Convert a linked list to a string.cpp
@@ -0,0 +1,37 @@
+// https://www.codewars.com/kata/582c297e56373f0426000098/train/cpp
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <list>
+#include <cmath>
+#include <iomanip>
+#include <map>
+using namespace std;
+
+class Node
+{
+  public:
+    int data;
+    Node* next;
+  
+  Node(int data, Node* next = nullptr)
+  {
+    this->data = data;
+    this->next = next;
+  }
+};
+
+#include <string>
+
+// Function to stringify the linked list
+std::string stringify(Node* list)
+{
+    // Base case: if the list is null, return "null"
+    if (list == nullptr) {
+        return "nullptr";
+    }
+
+    // Recursive case: return the current node's data and the result of stringify on the next node
+    return std::to_string(list->data) + " -> " + stringify(list->next);
+}
