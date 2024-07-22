bdff2552d6ca3adf7cdd05e56983329f960fd4c2
Edmont Budakyan <edmont@abv.bg>
2024-07-22 16:44:08 +0300

    add snippets

--- Codewars/7kyu/01.Basic, conditional, loops/Minimum Perimeter of a Rectangle.cpp
+++ Codewars/7kyu/01.Basic, conditional, loops/Minimum Perimeter of a Rectangle.cpp
@@ -27,3 +27,11 @@ int main() {
 
     return 0;
 }
+//
+#include <cmath>
+
+int minimumPerimeter (int area) {
+    int a = std::sqrt(area);
+    for (; a > 0 && area % a != 0; --a);
+    return 2 * (a + area / a);
+}
