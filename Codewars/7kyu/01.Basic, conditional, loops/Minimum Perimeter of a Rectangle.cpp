e010d009f161ef2ed39d432119adfd33b461eced
Edmont Budakyan <edmont@abv.bg>
2024-07-22 16:43:34 +0300

    add snippets

---
+++ Codewars/7kyu/01.Basic, conditional, loops/Minimum Perimeter of a Rectangle.cpp
@@ -0,0 +1,29 @@
+// https://www.codewars.com/kata/5826f54cc60c7e5266000baf/train/cpp
+#include <iostream>
+#include <climits>
+#include <cmath>
+using namespace std;
+
+int minimumPerimeter (int area)
+{
+  int minPerimeter = INT_MAX;
+  for (int height = 1; height <= std::sqrt(area); height++) {
+      if (area % height == 0) {
+          int width = area / height;
+          int perimeter = 2 * (height + width);
+          if (minPerimeter > perimeter) {
+              minPerimeter = perimeter;
+          }
+      }
+  }
+  return minPerimeter;
+}
+
+int main() {
+    cout << minimumPerimeter(45) << endl;  // 28
+    cout << minimumPerimeter(30) << endl;  // 22
+    cout << minimumPerimeter(81) << endl;  // 36
+    cout << minimumPerimeter(89) << endl;  // 180
+
+    return 0;
+}
