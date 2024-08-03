f8e872f94c151ab49216f6c68a065e39f4d4ddcc
Edmont Budakyan <edmont@abv.bg>
2024-08-03 20:52:14 +0300

    add snippets

--- Codewars/7kyu/05.String/Upturn Numeral Triangle.cpp
+++ Codewars/7kyu/05.String/Upturn Numeral Triangle.cpp
@@ -40,3 +40,22 @@ int main() {
 
     return 0;
 }
+//
+#include <string>
+#include <sstream>
+std::string pattern(int n)
+{
+	std::ostringstream oss;
+
+	for (int i = 1; i <= n; ++i)
+	{
+		oss << std::string(i - 1, ' ');
+
+		for (int j = i; j <= n; ++j)
+		{
+			oss << ' ' << i % 10;
+		}
+		if (i != n) oss << '\n';
+	}
+	return oss.str();
+}
