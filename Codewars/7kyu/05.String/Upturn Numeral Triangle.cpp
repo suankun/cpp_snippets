b261432735d732664d3e2e217472c6d1d50fe40c
Edmont Budakyan <edmont@abv.bg>
2024-08-03 20:50:19 +0300

    add snippets

---
+++ Codewars/7kyu/05.String/Upturn Numeral Triangle.cpp
@@ -0,0 +1,42 @@
+// https://www.codewars.com/kata/564f3d49a06556d27c000077/train/cpp
+#include <iostream>
+
+using namespace std;
+
+std::string pattern(int n)
+{
+    std::string output;
+    
+    int counter = 0, emptySpace = 1;
+    while (n--)
+    {
+        if (counter < 9)
+            counter++;
+        else
+            counter = 0;
+
+        output += std::string(emptySpace, ' ');
+
+        for (int curr = 0; curr <= n; curr++) {
+            output += std::to_string(counter);
+            if (curr < n)
+                output += ' ';
+        }
+
+
+        if (n > 0)
+            output += '\n';
+
+        emptySpace++;
+    }
+    
+    
+    return output;
+}
+
+int main() {
+    // cout << pattern(7) << endl;
+    cout << pattern(16) << endl;
+
+    return 0;
+}
