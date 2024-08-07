a8c73deeba01c63dd5c82071d61f89a85f51232d
Edmont Budakyan <edmont@abv.bg>
2024-08-07 21:17:54 +0300

    add snippets

---
+++ Codewars/7kyu/01.Basic, conditional, loops/Differential Averaging.cpp
@@ -0,0 +1,5 @@
+// https://www.codewars.com/kata/52c32ef251f31ae8f50000ae/train/cpp
+double addToAverage(double current, int points, double add)
+{
+    return (current * points + add) / (points + 1);
+}
