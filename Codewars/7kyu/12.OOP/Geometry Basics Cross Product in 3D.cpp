2220d39b5ac43c47a732b287225ee828fceded54
Edmont Budakyan <edmont@abv.bg>
2024-08-15 21:04:10 +0300

    add snippets

--- Codewars/7kyu/12.OOP/Geometry Basics Cross Product in 3D.cpp
+++ Codewars/7kyu/12.OOP/Geometry Basics Cross Product in 3D.cpp
@@ -12,3 +12,7 @@ Vector cross_product(const Vector& a, const Vector& b) {
 
     return Vector(x, y, z);
 }
+//
+Vector cross_product(const Vector& a, const Vector& b) {
+    return {a.y*b.z - a.z*b.y, a.z*b.x - a.x*b.z, a.x*b.y - a.y*b.x};
+}
