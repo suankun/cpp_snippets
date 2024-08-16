e0017a7c49aab6591209744306f0cd4fa2be3c71
Edmont Budakyan <edmont@abv.bg>
2024-08-16 16:44:06 +0300

    add snippets

--- Codewars/7kyu/10.Pair, map, set, tuple/Splitting Numbers.cpp
+++ Codewars/7kyu/10.Pair, map, set, tuple/Splitting Numbers.cpp
@@ -21,3 +21,9 @@ std::pair<int, int> splitNumbers(int n) {
 
     return std::make_pair(a, b);
 }
+//
+std::pair<int, int> splitNumbers( int n ) {
+    int a{}, b{}, i{};
+    while ( i < 31 ) ( a > b ? b : a ) |= n & 1 << i++;
+    return { a, b };
+}
