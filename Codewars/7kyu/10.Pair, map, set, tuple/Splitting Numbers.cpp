d97cdc917dd28b77a0ce9379e9cb51dabe08e639
Edmont Budakyan <edmont@abv.bg>
2024-08-16 16:43:49 +0300

    add snippets

---
+++ Codewars/7kyu/10.Pair, map, set, tuple/Splitting Numbers.cpp
@@ -0,0 +1,23 @@
+// https://www.codewars.com/kata/5b7d72b77a2013975700015c/train/cpp
+#include <utility>
+
+std::pair<int, int> splitNumbers(int n) {
+    int a = 0, b = 0;
+    bool toggle = true;
+    int bitPosition = 0;
+
+    while (n > 0) {
+        if (n & 1) {
+            if (toggle) {
+                a |= (1 << bitPosition);
+            } else {
+                b |= (1 << bitPosition);
+            }
+            toggle = !toggle;
+        }
+        n >>= 1; // Move to the next bit
+        bitPosition++;
+    }
+
+    return std::make_pair(a, b);
+}
