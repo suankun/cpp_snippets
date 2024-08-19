ffdb258d9d788d8c666e158e697265a9516650cd
Edmont Budakyan <edmont@abv.bg>
2024-08-19 09:48:02 +0300

    add snippets

---
+++ Lecture.h
@@ -0,0 +1,16 @@
+#ifndef LECTURE_H
+#define LECTURE_H
+
+#include "ResourceType.h"
+
+namespace SoftUni {
+
+    class Lecture {
+
+    };
+
+
+}
+
+
+#endif // !LECTURE_H
---
+++ Resource.h
@@ -0,0 +1,16 @@
+#ifndef RESOURCE_H
+#define RESOURCE_H
+
+#include "ResourceType.h"
+
+namespace SoftUni {
+
+    class Resource {
+
+    };
+
+
+}
+
+
+#endif // !RESOURCE_H
