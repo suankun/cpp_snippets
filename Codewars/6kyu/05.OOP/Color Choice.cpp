2c40245870e93e8627db709c94159f634093b4b0
Edmont Budakyan <edmont@abv.bg>
2024-08-31 20:21:06 +0300

    add snippets

--- Codewars/6kyu/05.OOP/Color Choice.cpp
+++ Codewars/6kyu/05.OOP/Color Choice.cpp
@@ -42,3 +42,20 @@ public:
         return -1;
     }
 };
+//
+class ColorChoice
+{
+public:
+    static long long checkChoose(long long m, int n);
+};
+
+long long ColorChoice::checkChoose(long long m, int n)
+{
+    long long result = 1;
+    for (int i = 0; i <= n; ++i)
+    {
+        if (result == m) return i;
+        result = result * (n - i) / (i + 1);
+    }
+    return -1;
+}
