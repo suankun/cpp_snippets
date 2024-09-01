9456cd07c9742e749fbbd3e117d20e4af0685420
Edmont Budakyan <edmont@abv.bg>
2024-09-01 22:29:45 +0300

    add snippet

---
+++ Codewars/6kyu/01.Basic/Progressive Spiral Number Distance.cpp
@@ -0,0 +1,20 @@
+// https://www.codewars.com/kata/5a27e3438882f334a10000e3/train/cpp
+#include <cmath>
+
+int distance(int n) {
+    if (n == 1) return 0;
+    
+    int layer = std::ceil((std::sqrt(n) - 1) / 2);
+    
+    int maxInLayer = (2 * layer + 1) * (2 * layer + 1);
+    
+    int sideLen = 2 * layer;
+    
+    int stepsFromMax = maxInLayer - n;
+    
+    int positionInLayer = stepsFromMax % sideLen;
+    
+    int midpointOffset = std::abs(positionInLayer - layer);
+    
+    return layer + midpointOffset;
+}
