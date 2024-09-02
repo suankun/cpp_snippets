609bb49b62e64ca4ebea3858b1d9e09ef08a8423
Edmont Budakyan <edmont@abv.bg>
2024-09-02 22:36:05 +0300

    add snippet

---
+++ Codewars/7kyu/01.Basic, conditional, loops/A snail enters a bar!.cpp
@@ -0,0 +1,5 @@
+// https://www.codewars.com/kata/66cdc6ab9e7a9f009e0ca8f6/train/cpp
+bool canSnailReachEnd(double length, double speed, double lengthIncreases)
+{
+    return (speed - lengthIncreases) * 60 * 24 * 365 > length ? true : false;
+}
