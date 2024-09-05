0d26367f90f479d384d568c78b714a89ac8d6136
Edmont Budakyan <edmont@abv.bg>
2024-09-05 09:09:06 +0300

    add snippets

--- Codewars/6kyu/01.Basic/Points in the circle.cpp
+++ Codewars/6kyu/01.Basic/Points in the circle.cpp
@@ -20,4 +20,11 @@ static int pointsNumber(int radius)
     }
     return totalPoints;
 }
-  
+//
+static int pointsNumber( int radius ) {
+    radius *= radius;
+    int count{}; // one quadrant
+    for ( int i{ 1 }; i <= radius; i += 4 )
+        count += radius / i - radius / ( i + 2 );
+    return 4 * count + 1;
+}
