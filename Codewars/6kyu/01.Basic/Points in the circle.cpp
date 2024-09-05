a5bb684409fe68e591ce09006d15726bbd17ac14
Edmont Budakyan <edmont@abv.bg>
2024-09-05 09:08:31 +0300

    add snippets

---
+++ Codewars/6kyu/01.Basic/Points in the circle.cpp
@@ -0,0 +1,23 @@
+// https://www.codewars.com/kata/5b55c49d4a317adff500015f/train/cpp
+#include <cmath>
+
+double distance(int x1, int y1, int x2, int y2)
+{
+    return std::sqrt(((x2 - x1) * (x2 - x1)) + ((y2 - y1) * (y2 - y1)));
+}
+
+static int pointsNumber(int radius)
+{
+    int totalPoints = 0;
+    double rDist = radius;
+    for(int i = -radius; i <= radius; i++)
+    {
+        for(int j = -radius; j <= radius; j++)
+        {
+            if(distance(i, j, 0, 0) <= rDist)
+                totalPoints++;
+        }
+    }
+    return totalPoints;
+}
+  
