521eb4c915b97240a16e93638f4d376241281ca8
Edmont Budakyan <edmont@abv.bg>
2024-09-17 08:30:15 +0300

    add snippets

--- Codewars/6kyu/03.String/Grouped by commas.cpp
+++ Codewars/6kyu/03.String/Grouped by commas.cpp
@@ -35,3 +35,25 @@ int main() {
 
     return 0;
 }
+//
+#include <string>
+#include <locale>
+#include <sstream>
+#include <memory>
+
+struct separate_thousands : std::numpunct<char> {
+    char_type do_thousands_sep() const override { return ','; }  // separate with commas
+    string_type do_grouping() const override { return "\3"; } // groups of 3 digit
+};
+
+std::string group_by_commas(int n)
+{
+    auto thousands = std::make_unique<separate_thousands>();
+    std::stringstream str;
+    str.imbue(std::locale(str.getloc(), thousands.release()));
+    
+    str << n;
+    std::string res;
+    str >> res;
+    return res;
+}
