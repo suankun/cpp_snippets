0ea1c0fd15c8ab28775cba98da5393635029abbd
Edmont Budakyan <edmont@abv.bg>
2024-09-18 21:49:33 +0300

    add snippets

---
+++ Codewars/6kyu/04.Vector/Find the missing term in an Arithmetic Progression.cpp
@@ -0,0 +1,21 @@
+// https://www.codewars.com/kata/52de553ebb55d1fca3000371/train/cpp
+#include <iostream>
+#include <vector>
+#include <numeric>
+
+static long findMissing(std::vector<long> list) {
+    long n = list.size() + 1;
+    long expected_sum = (list[0] + list.back()) * n / 2;
+    
+    long actual_sum = std::accumulate(list.begin(), list.end(), 0L);
+    
+    return expected_sum - actual_sum;
+}
+
+int main() {
+    std::vector<long> list = {-23, -19, -15, -11}; // The missing number is -17
+    
+    std::cout << "The missing number is: " << findMissing(list) << std::endl;
+
+    return 0;
+}
