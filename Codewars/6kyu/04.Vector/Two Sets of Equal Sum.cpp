56a963f9efdc0b8d6cf30d8c721bf276d085f557
Edmont Budakyan <edmont@abv.bg>
2024-09-20 12:46:28 +0300

    add snippets

--- Codewars/6kyu/04.Vector/Two Sets of Equal Sum.cpp
+++ Codewars/6kyu/04.Vector/Two Sets of Equal Sum.cpp
@@ -12,25 +12,25 @@ std::array<std::vector<int>, 2> createTwoSetsOfEqualSum(int n) {
         return sets;
 
     if (n%4!=0) {
-    sets[0].push_back(1);
-    sets[0].push_back(2);
-    sets[1].push_back (3);
-      if (n>3) {
-        for (int i=4;i<n;) {
-          sets[0].push_back(i++);
-          sets[1].push_back(i++);
-          sets[1].push_back(i++);
-          sets[0].push_back(i++);
+        sets[0].push_back(1);
+        sets[0].push_back(2);
+        sets[1].push_back (3);
+        if (n>3) {
+            for (int i=4;i<n;) {
+                sets[0].push_back(i++);
+                sets[1].push_back(i++);
+                sets[1].push_back(i++);
+                sets[0].push_back(i++);
+            }
         }
-      }
     }
     else{
-      for (int i=1;i<n;) {
-        sets[0].push_back(i++);
-        sets[1].push_back(i++);
-        sets[1].push_back(i++);
-        sets[0].push_back(i++);
-      }
+        for (int i=1;i<n;) {
+            sets[0].push_back(i++);
+            sets[1].push_back(i++);
+            sets[1].push_back(i++);
+            sets[0].push_back(i++);
+        }
     }
   
     return sets;
@@ -47,4 +47,20 @@ int main() {
     cout << endl;
 
     return 0;
+}
+//
+#include <array>
+#include <vector>
+
+std::array<std::vector<int>, 2> createTwoSetsOfEqualSum(int n) {
+    std::array<std::vector<int>, 2> sets;
+    if (n % 4 == 0 || n % 4 == 3) {
+        std::array<long, 2> sums{};
+        for (int i = n; i > 0; i--) {
+            bool j = sums[0] > sums[1];
+            sets[j].push_back(i);
+            sums[j] += i;
+        }
+    }
+    return sets;
 }
