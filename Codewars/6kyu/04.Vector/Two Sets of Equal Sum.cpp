294c725579121369b43ae818add1318ff67473d7
Edmont Budakyan <edmont@abv.bg>
2024-09-20 12:44:29 +0300

    add snippets

---
+++ Codewars/6kyu/04.Vector/Two Sets of Equal Sum.cpp
@@ -0,0 +1,50 @@
+// https://www.codewars.com/kata/647518391e258e80eedf6e06/train/cpp
+#include <iostream>
+#include <vector>
+#include <array>
+
+using namespace std;
+
+std::array<std::vector<int>, 2> createTwoSetsOfEqualSum(int n) {
+    std::array<std::vector<int>, 2> sets;
+
+    if (n % 4 != 0 && (n + 1) % 4 != 0)
+        return sets;
+
+    if (n%4!=0) {
+    sets[0].push_back(1);
+    sets[0].push_back(2);
+    sets[1].push_back (3);
+      if (n>3) {
+        for (int i=4;i<n;) {
+          sets[0].push_back(i++);
+          sets[1].push_back(i++);
+          sets[1].push_back(i++);
+          sets[0].push_back(i++);
+        }
+      }
+    }
+    else{
+      for (int i=1;i<n;) {
+        sets[0].push_back(i++);
+        sets[1].push_back(i++);
+        sets[1].push_back(i++);
+        sets[0].push_back(i++);
+      }
+    }
+  
+    return sets;
+}
+
+int main() {
+    std::array<std::vector<int>, 2> res = createTwoSetsOfEqualSum(3);
+    for (auto n : res[0])
+        cout << n << ' ';
+    cout << endl;
+
+    for (auto n : res[1])
+        cout << n << ' ';
+    cout << endl;
+
+    return 0;
+}
