b4100e98a07fbe01d8f59fede503572e531ec436
Edmont Budakyan <edmont@abv.bg>
2024-09-26 21:14:27 +0300

    add snippets

--- Codewars/6kyu/06.Map, Pair/Decode the Morse code.cpp
+++ Codewars/6kyu/06.Map, Pair/Decode the Morse code.cpp
@@ -70,3 +70,21 @@ int main() {
 
     return 0;
 }
+//
+// #include <regex>
+
+// const std::regex morse("([^\\s]+|\\s{3})");
+
+// std::string decodeMorse(std::string morseCode) {
+//     std::string decoded;
+    
+//     auto begin = morseCode.begin() + morseCode.find_first_not_of(' ');
+//     auto end = morseCode.begin() + morseCode.find_last_not_of(' ') + 1;
+    
+//     for (auto it = std::sregex_iterator(begin, end, morse); it != std::sregex_iterator(); ++it) {
+//         std::string str = it->str();
+//         decoded += str == "   " ? " " : MORSE_CODE[str];
+//     }
+    
+//     return decoded;
+// }
