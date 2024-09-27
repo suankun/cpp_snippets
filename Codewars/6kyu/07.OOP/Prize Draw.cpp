0d0827d644820536589ac75db2b42f9200606160
Edmont Budakyan <edmont@abv.bg>
2024-09-27 14:57:38 +0300

    add snippets

--- Codewars/6kyu/07.OOP/Prize Draw.cpp
+++ Codewars/6kyu/07.OOP/Prize Draw.cpp
@@ -111,3 +111,54 @@ int main()
 
     return 0;
 }
+//
+#include <string>
+#include <vector>
+#include <sstream>
+#include <utility>
+#include <algorithm>
+#include <ctime>
+#include <chrono>
+#include <random>
+
+class Rank
+{
+private:
+    static std::vector<std::string> split(const std::string &s);
+    static bool compare(const std::pair<std::string, long long> &s1, const std::pair<std::string, long long> &s2);
+public:
+    static std::string nthRank(const std::string &st, std::vector<int> &we, int n);
+};
+
+std::vector<std::string> Rank::split(const std::string &s)
+{
+    std::vector<std::string> out;
+    std::istringstream ss(s);
+    std::string next;
+    while (std::getline(ss, next, ','))
+        out.push_back(next);
+    return out;
+}
+bool Rank::compare(const std::pair<std::string, long long> &s1, const std::pair<std::string, long long> &s2)
+{
+    long long w1 = s1.second; long long w2 = s2.second;
+    if (w1 == w2) return s1.first < s2.first;
+    return w1 > w2;
+}
+std::string Rank::nthRank(const std::string &st, std::vector<int> &we, int n)
+{
+    if (st == "") return "No participants";
+    std::vector<std::string> names = split(st);
+    if (n > static_cast<int>(names.size())) return "Not enough participants";
+
+    std::vector<std::pair <std::string, long long>> persons;
+    for (unsigned int i = 0; i < names.size(); i++)
+    {
+        unsigned int weight = names[i].length();
+        for (auto ch : names[i])
+            weight += std::toupper(ch) - 64;
+        persons.push_back(std::make_pair(names[i], static_cast<long long>(weight * we[i])));
+    }
+    std::sort (persons.begin(), persons.end(), compare);
+    return persons[n - 1].first;
+}
