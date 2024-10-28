7069336fbeecbc3fb5b4a910d4da201bd659c523
Edmont Budakyan <edmont@abv.bg>
2024-10-28 21:19:39 +0200

    add snippets

---
+++ Codewars/6kyu/01.Basic/Calculate the area of a regular n sides polygon inside a circle of radius r.cpp
@@ -0,0 +1,28 @@
+// https://www.codewars.com/kata/5a58ca28e626c55ae000018a/train/cpp
+// Write the following function:
+
+// double areaOfPolygonInsideCircle (double circleRadius , int numberOfSides)
+// It should calculate the area of a regular polygon of numberOfSides, number-of-sides, or number_of_sides sides inside a circle of radius circleRadius, circle-radius, or circle_radius which passes through all the vertices of the polygon (such circle is called circumscribed circle or circumcircle).
+
+// The answer should be a number rounded to 3 decimal places.
+
+// Input :: Output Examples
+
+// areaOfPolygonInsideCircle (3, 3) // returns 11.691
+
+// areaOfPolygonInsideCircle (5.8, 7) // returns 92.053
+
+// areaOfPolygonInsideCircle (4, 5) // returns 38.042
+// Note: if you need to use Pi in your code, use the native value of your language unless stated otherwise.
+
+#include <cmath>
+#include <iomanip>
+
+using namespace std;
+
+double areaOfPolygonInsideCircle(double circleRadius, int numberOfSides) {
+    const double PI = 3.14159265;
+    double angle = PI / numberOfSides;
+    double area = circleRadius * circleRadius * numberOfSides * sin(2 * angle) / 2;
+    return round(area * 1000) / 1000;
+}
