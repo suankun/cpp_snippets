f7674746bf2f46f00ecd2f801929e030cce036a1
Edmont Budakyan <edmont@abv.bg>
2024-11-04 10:41:53 +0200

    add snippets

--- Codewars/6kyu/03.String/Stop gninnipS My sdroW!.cpp
+++ Codewars/6kyu/03.String/Stop gninnipS My sdroW!.cpp
@@ -54,3 +54,20 @@ int main() {
 
     return 0;
 }
+#include <string>
+#include <algorithm>
+#include <sstream>
+std::string spinWords(const std::string &str)
+{
+    std::stringstream ss(str);
+    std::string result;
+    std::string buff;
+    while (ss >> buff) {
+        if (buff.size() >= 5) {
+            std::reverse(buff.begin(), buff.end());
+        }
+        result += buff + ' ';
+    }
+    result.pop_back();
+    return result;
+}
