f36f9d9449ba5fe1c1e5e92cc0e919e3a132d5d2
Edmont Budakyan <edmont@abv.bg>
2024-11-06 10:04:12 +0200

    add snippets

--- Codewars/6kyu/04.Vector/Are they the same.cpp
+++ Codewars/6kyu/04.Vector/Are they the same.cpp
@@ -83,3 +83,19 @@ int main() {
 
     return 0;
 }
+//
+#include <algorithm>
+
+class Same {
+public:
+    static bool comp(std::vector<int>, std::vector<int>);
+};
+
+bool Same::comp(std::vector<int> a, std::vector<int> b) {
+    for (auto& v : a) {
+        v = v * v;
+    }
+    std::sort(a.begin(), a.end());
+    std::sort(b.begin(), b.end());
+    return a == b;
+}
