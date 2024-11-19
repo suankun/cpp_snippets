fdfaef990dc1c8acc0496439380af553f948abe8
Edmont Budakyan <edmont@abv.bg>
2024-11-19 20:19:19 +0200

    add snippets

--- Codewars/6kyu/04.Vector/Roman Numerals Decoder.cpp
+++ Codewars/6kyu/04.Vector/Roman Numerals Decoder.cpp
@@ -59,3 +59,30 @@ int main() {
 
     return 0;
 }
+//
+#include <iostream>
+#include <string>
+#include <map>
+
+using namespace std;
+
+map<char, int> nums = {{'M', 1000}, {'D', 500}, {'C', 100}, {'L', 50}, {'X', 10}, {'V', 5}, {'I', 1}};
+
+int solution(string roman) {
+    int res = 0;
+    int old = 0;
+    
+    for(auto a : roman){
+        int curr = nums[a];
+        res += curr;
+        
+        if(curr > old){
+            res -= 2 * old;
+        }
+        
+        old = curr;
+    }
+    
+    
+    return res;
+}
