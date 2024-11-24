581dad013761e9a31f4030d9a23d8a7601faf75d
Edmont Budakyan <edmont@abv.bg>
2024-11-24 18:44:40 +0200

    add snippets

--- Codewars/6kyu/07.OOP/Reverse or rotate.cpp
+++ Codewars/6kyu/07.OOP/Reverse or rotate.cpp
@@ -81,3 +81,26 @@ int main() {
 
     return 0;
 }
+//
+// #include <string>
+// #include <algorithm>
+
+// class RevRot
+// {
+// public:
+//     static std::string revRot(const std::string &strng, unsigned int sz){
+//         if(sz <= 0 || strng.empty() || sz > strng.length())
+//             return "";
+//         std::string x = strng.substr(0,sz);
+//         bool p = true;
+//         for(const char& c : x)
+//             if(c % 2 == 1) // '0' == 48, so 'digit' is even iff digit is even 
+//             p = !p;
+//         if(p){
+//             std::reverse(x.begin(),x.end());
+//             return x + revRot(strng.substr(sz),sz);
+//         }
+//         x += x[0];
+//         return x.substr(1) + revRot(strng.substr(sz),sz);
+//     }
+// };
