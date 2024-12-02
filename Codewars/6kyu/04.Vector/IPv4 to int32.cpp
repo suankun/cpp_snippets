8423975080640f7632b62b495450ebebbaa21b6e
Edmont Budakyan <edmont@abv.bg>
2024-12-02 09:56:07 +0200

    add snippets

--- Codewars/6kyu/04.Vector/IPv4 to int32.cpp
+++ Codewars/6kyu/04.Vector/IPv4 to int32.cpp
@@ -44,3 +44,13 @@ int main() {
 
     return 0;
 }
+//
+#include <cstdint>
+#include <string>
+#include <cmath>
+
+uint32_t ip_to_int32(const std::string& ip) {
+    int a,b,c,d;
+    sscanf(ip.c_str(),"%i.%i.%i.%i",&a,&b,&c,&d);
+    return a*pow(2,24) + b*pow(2,16) + c*pow(2,8) + d;
+}
