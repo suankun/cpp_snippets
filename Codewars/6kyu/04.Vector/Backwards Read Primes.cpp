91c4825e849d316977a475f830d7e191ea3744bf
Edmont Budakyan <edmont@abv.bg>
2024-12-08 21:47:50 +0200

    add snippets

--- Codewars/6kyu/04.Vector/Backwards Read Primes.cpp
+++ Codewars/6kyu/04.Vector/Backwards Read Primes.cpp
@@ -78,3 +78,34 @@ std::string BackWardsPrime::backwardsPrime(long long start, long long end)
     }
     return oss.str();
 }
+//
+// #include <cmath>
+
+// using namespace std;
+// typedef long long ll;
+
+// class BackWardsPrime {
+//     public:
+//     static string backwardsPrime(ll start, ll end);
+// };
+
+// bool is_prime(ll n) {
+//     if (n == 2) return true;
+//     if (n % 2 == 0) return false;
+//     int root = sqrt(n);
+//     for (int i = 3; i < root + 1; i += 2)
+//         if ((n % i) == 0) return false;
+//     return true;
+// }
+
+// string BackWardsPrime::backwardsPrime(ll start, ll end) {
+//     string ret;
+//     for (ll i = start; i <= end; ++i) {
+//         string i_str = to_string(i);
+//         reverse(i_str.begin(), i_str.end());
+//         ll i_reversed = stoll(i_str);
+//         if (i != i_reversed && is_prime(i) && is_prime(i_reversed))
+//         ret += to_string(i) + " ";
+//     }
+//     return ret.substr(0, ret.size() - 1);
+// }
