468ee983a874ca7545935fa6a3a6c294e1745953
Edmont Budakyan <edmont@abv.bg>
2024-12-11 22:30:47 +0200

    add snippets

--- Codewars/6kyu/03.String/The Deaf Rats of Hamelin.cpp
+++ Codewars/6kyu/03.String/The Deaf Rats of Hamelin.cpp
@@ -58,3 +58,17 @@ int main() {
 
     return 0;
 }
+//
+// int countDeafRats(const std::string &town)
+// {
+//     std::string clean{town};
+//     clean.erase(remove_if(clean.begin(), clean.end(), isspace), clean.end()); // erase whitespace
+//     int res = 0;
+    
+//     // No matter where the flute, O must always appear on odd positions
+//     // If not, the rat is facing the wrong way.
+//     for(int i = 1; i< clean.length(); i+=2) { 
+//         if(clean[i] != 'O') { res ++; }
+//     }
+//     return res;
+// }
