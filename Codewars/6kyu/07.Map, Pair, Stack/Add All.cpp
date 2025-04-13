a200abb6ce493a549d6c71c5e06b531648416d1a
Edmont Budakyan <edmont@abv.bg>
2025-04-13 14:39:04 +0300

    add snippets

--- Codewars/6kyu/07.Map, Pair, Stack/Add All.cpp
+++ Codewars/6kyu/07.Map, Pair, Stack/Add All.cpp
@@ -36,3 +36,16 @@ int addAll(const std::vector<int>& numbers) {
 
     return totalCost;
 }
+//
+#include <queue>
+int addAll(const std::vector<int> & numbers) {
+    std::priority_queue q{std::greater{}, numbers};
+    int cost{};
+    while (q.size() > 1) {
+        int sum{q.top()};
+        q.pop(), cost += sum += q.top();
+        q.pop(), q.push(sum);
+    }
+
+    return cost;
+}
